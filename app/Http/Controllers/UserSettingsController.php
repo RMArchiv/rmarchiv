a94778d421146950fff4888f223dba6f56b04001
Marcel Hering <broxxer@broxxer.de>
2016-11-28 11:05:32 +0100

    add: usersettings controller and view

--- app/Http/Controllers/UserSettingsController.php
+++ app/Http/Controllers/UserSettingsController.php
@@ -13,7 +13,7 @@ class UserSettingsController extends Controller
      */
     public function index()
     {
-        //
+        return view('auth.settings');
     }
 
     /**
---
+++ resources/views/auth/settings.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+    Kommt noch. Abgesehen vom AVA weiß ich noch nicht was hier rein soll... xD
+@endsection
