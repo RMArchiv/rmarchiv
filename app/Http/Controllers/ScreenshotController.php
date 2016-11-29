567bee23a6043f99be57d71a510b513f6bac10f4
Marcel Hering <broxxer@broxxer.de>
2016-11-29 11:42:14 +0100

    add: Screenshot upload View

--- app/Http/Controllers/ScreenshotController.php
+++ app/Http/Controllers/ScreenshotController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Intervention\Image\Facades\Image;
+use Illuminate\Http\UploadedFile;
+
 
 class ScreenshotController extends Controller
 {
@@ -26,10 +28,33 @@ class ScreenshotController extends Controller
     }
 
     public function create($gameid, $screenid){
-
+        return view('screenshots.create', [
+            'gameid' => $gameid,
+            'screenid' => $screenid,
+        ]);
     }
 
-    public function upload($gameid, $screenid){
+    public function upload(Request $request, $gameid, $screenid){
+        $this->validate($request, [
+            'file' => 'required|image|mimes:png|max:2048',
+        ]);
 
+        $file = $request->file('file');
+        $ext = $file->getClientOriginalExtension();
+        $extorig = $file->getExtension();
+
+        $imageName = \Storage::putFile('screenshots', new UploadedFile($file->path(), $file->getClientOriginalName()));
+        //dd($file);
+
+        $l = new Logo;
+        $l->extension = \Storage::mimeType($imageName);
+        $l->filename = str_replace($extorig, '', $imageName);
+        $l->title = $request->get('logoname');
+        $l->user_id = \Auth::id();
+
+
+        $l->save();
+
+        return redirect()->route('submit.logo.success');
     }
 }
---
+++ resources/views/screenshots/create.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+    <div id="content">
+        <form action="{{ route('screenshot.upload', [$gameid, $screenid]) }}" method="post" enctype="multipart/form-data">
+            {{ csrf_field() }}
+
+            <div class="rmarchivtbl" id="rmarchivbox_submitscreenshot">
+                <h2>Screenshot hochladen</h2>
+
+                @if (count($errors) > 0))
+                <div class="rmarchivtbl errorbox">
+                    <h2>Screenshot hochladen fehlgeschlagen</h2>
+                    <div class="content">
+                        @foreach ($errors->all() as $error)
+                            <strong>{{ $error }}</strong>
+                        @endforeach
+                    </div>
+                </div>
+                @endif
+
+                <div class="content">
+                    <div class="formifier">
+                        <div class="row" id="row_file">
+                            <label for="file">Screenshot:</label>
+                            <input name="file" id="file" type="file" value=""/>
+                            <span> [<span class="req">req</span>] nur PNG!</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="foot">
+                    <input type="submit" value="{{ trans('app.misc.send') }}">
+                </div>
+            </div>
+        </form>
+    </div>
+@endsection
