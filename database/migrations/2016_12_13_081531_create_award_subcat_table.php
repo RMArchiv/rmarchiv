175fe905881d5fa70b525c9583bf74acdf8aa6bf
Marcel Hering <broxxer@broxxer.de>
2016-12-13 08:48:52 +0100

    del: Ressourcen Migrationen (kommen erst später dran) add: Award Migrationen

--- database/migrations/2016_12_13_081531_create_award_subcat_table.php
+++ database/migrations/2016_12_13_081531_create_award_subcat_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateResourcesPacksRelationTable extends Migration
+class CreateAwardSubcatTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,12 @@ class CreateResourcesPacksRelationTable extends Migration
      */
     public function up()
     {
-        Schema::create('resources_packs_relation', function (Blueprint $table) {
+        Schema::create('award_subcats', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title');
+            $table->string('desc_html', 2000);
+            $table->string('desc_md', 1024);
+
             $table->timestamps();
         });
     }
@@ -26,6 +30,6 @@ class CreateResourcesPacksRelationTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('resources_packs_relation');
+        Schema::dropIfExists('award_subcats');
     }
 }
--- database/migrations/2016_12_13_081623_add_award_subcat_id_to_games_awards_table.php
+++ database/migrations/2016_12_13_081623_add_award_subcat_id_to_games_awards_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateResourcesTable extends Migration
+class AddAwardSubcatIdToGamesAwardsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,8 @@ class CreateResourcesTable extends Migration
      */
     public function up()
     {
-        Schema::create('resources', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
+        Schema::table('games_awards', function (Blueprint $table) {
+            $table->integer('award_subcat_id');
         });
     }
 
@@ -26,6 +25,8 @@ class CreateResourcesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('resources');
+        Schema::table('games_awards', function (Blueprint $table) {
+            $table->dropColumn('award_subcat_id');
+        });
     }
 }
