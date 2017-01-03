4dd85e40f71229bb2745a8b194cbe67dc1dc9c3a
Marcel Hering <broxxer@broxxer.de>
2017-01-03 08:13:01 +0100

    fix: Im Header war der Link zum User der das Logo erstellt hat defekt.

--- resources/views/_partials/header.blade.php
+++ resources/views/_partials/header.blade.php
@@ -2,6 +2,6 @@
     <h1>{{ trans('app.title') }}</h1>
     <div id='logo'>
         <a href="/"><img height="100px" src="{{ asset($logo->filename) }}" alt="Logo: {{ $logo->title }}"/></a>
-        <p>logo '{{ $logo->title }}' by <a href='pfad' class='user'>{{ $logo->name }}</a> :: {{ config('app.name') }} is brought to you by <a href="{{ url('users', 1) }}">ryg</a></p>
+        <p>logo '{{ $logo->title }}' by <a href='{{ url('users', $logo->id) }}' class='user'>{{ $logo->name }}</a> :: {{ config('app.name') }} is brought to you by <a href="{{ url('users', 1) }}">ryg</a></p>
     </div>
 </header>
