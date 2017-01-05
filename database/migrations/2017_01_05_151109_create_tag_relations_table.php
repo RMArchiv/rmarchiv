ba0c3fc11bc93d03485b3b3426601d8ed25f5382
Marcel Hering <broxxer@broxxer.de>
2017-01-05 15:15:51 +0100

    Migrationen für Tagging System.

---
+++ database/migrations/2017_01_05_151109_create_tag_relations_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagRelationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tag_relations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('tag_id');
+            $table->integer('user_id');
+            $table->integer('content_id');
+            $table->string('content_type');
+            $table->timestamps();
+
+            $table->index('tag_id');
+            $table->index('user_id');
+            $table->index('content_id');
+            $table->index('content_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tag_relations');
+    }
+}
