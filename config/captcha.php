23c7ffbfc68dbdce9fd33332448a4e9d6e0a4e20
Marcel Hering <broxxer@broxxer.de>
2017-01-19 14:07:32 +0100

    - Captcha für Registrierung - Spiele können gelöscht werden

---
+++ config/captcha.php
@@ -0,0 +1,60 @@
+<?php
+
+return [
+
+    'characters' => '0123456789',
+
+    'default'   => [
+        'length'    => 5,
+        'width'     => 120,
+        'height'    => 36,
+        'quality'   => 90,
+    ],
+
+    'rmarchiv' => [
+        'length'    => 6,
+        'width'     => 160,
+        'height'    => 46,
+        'quality'   => 100,
+        'lines'     => 6,
+        'bgImage'   => false,
+        'bgColor'   => '#17395C',
+        'fontColors'=> ['#ffbf00', '#ffdf00', '#ffffe0', '#4E6F90', '#FF8888'],
+        'contrast'  => 0,
+        'sensitive' => true,
+        'angle'     => 12,
+        'invert'    => false,
+    ],
+
+    'flat'   => [
+        'length'    => 6,
+        'width'     => 160,
+        'height'    => 46,
+        'quality'   => 90,
+        'lines'     => 6,
+        'bgImage'   => false,
+        'bgColor'   => '#ecf2f4',
+        'fontColors'=> ['#2c3e50', '#c0392b', '#16a085', '#c0392b', '#8e44ad', '#303f9f', '#f57c00', '#795548'],
+        'contrast'  => -5,
+    ],
+
+    'mini'   => [
+        'length'    => 3,
+        'width'     => 60,
+        'height'    => 32,
+    ],
+
+    'inverse'   => [
+        'length'    => 5,
+        'width'     => 120,
+        'height'    => 36,
+        'quality'   => 90,
+        'sensitive' => true,
+        'angle'     => 12,
+        'sharpen'   => 10,
+        'blur'      => 2,
+        'invert'    => true,
+        'contrast'  => -5,
+    ]
+
+];
