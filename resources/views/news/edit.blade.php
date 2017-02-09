6f2bf4cc72d9a1f3a5758ffd5731fc19c8c72b2e
Marcel Hering <broxxer@broxxer.de>
2017-02-09 15:30:02 +0100

    Erste Schritte für Editor mit Vorschau!

--- resources/views/news/edit.blade.php
+++ resources/views/news/edit.blade.php
@@ -28,19 +28,9 @@
                                 <input name="title" id="title" value="{{ $news->title }}"/>
                                 <span> [<span class="req">req</span>]</span>
                             </div>
-                            <div class="row" id="row_message">
-                                <label for="msg">beschreibung:</label>
-                                <textarea name="msg" id="msg" maxlength="9999" rows="10"
-                                          placeholder="Newsbeitrag">{{ $news->news_md }}</textarea>
-                                <span> [<span class="req">req</span>] Markdown!</span>
-                            </div>
-                            <script type="text/javascript">
-                                $(function () {
-                                    $('textarea').inlineattachment({
-                                        uploadUrl: 'http://rmarchiv.de/attachment/upload',
-                                    });
-                                });
-                            </script>
+
+                            @include('_partials.markdown_editor', ['edit_text' => $news->news_md])
+
                             <div class="row" id="row_msg">
                                 <label for="cat">kategorie:</label>
                                 <input name="cat" id="cat" value="{{ $news->news_category }}" placeholder="allgemein"/>
@@ -51,29 +41,6 @@
                     <div class="foot">
                         <input type="submit" value="news speichern">
                     </div>
-                </div>
-
-                <div class="rmarchivtbl rmarchivbox_newsbox" id="rmarchivbox_prodmain">
-                    <h2>live preview</h2>
-                    <div class="content" id="preview_box">
-
-                    </div>
-                    <script>
-                        var reader = new commonmark.Parser();
-                        var writer = new commonmark.HtmlRenderer();
-                        var parsed = reader.parse(document.getElementById('msg').value);
-                        var result = writer.render(parsed);
-                        document.getElementById('preview_box').innerHTML = result;
-
-                        document.getElementById('msg').onkeyup = function () {
-                            var reader = new commonmark.Parser();
-                            var writer = new commonmark.HtmlRenderer();
-                            var parsed = reader.parse(document.getElementById('msg').value);
-                            var result = writer.render(parsed);
-                            document.getElementById('preview_box').innerHTML = result;
-                        }
-                    </script>
-
                 </div>
             </form>
 
