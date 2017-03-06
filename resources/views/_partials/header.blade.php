b99ae53b8b52365b401908bea49c7a4de63015a9
Marcel Hering <rygos@icloud.com>
2017-03-06 16:53:21 +0100

    header fix?

--- resources/views/_partials/header.blade.php
+++ resources/views/_partials/header.blade.php
@@ -1,7 +1,7 @@
 <header>
     <h1>{{ trans('app.title') }}</h1>
     <div id='logo'>
-        <a href="/"><img height="100px" src="{{ asset($logo->filename) }}" alt="Logo: {{ $logo->title }}"/></a>
+        <a href="/"><img height="100px" src="{{ storage_path('app/logos/'.$logo->filename) }}" alt="Logo: {{ $logo->title }}"/></a>
         <p>logo '{{ $logo->title }}' by <a href='{{ url('users', $logo->id) }}' class='user'>{{ $logo->name }}</a> :: {{ config('app.name') }} is brought to you with love.</p>
     </div>
 </header>
