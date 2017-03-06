b0d5e93496f099d917af64ad90fe412095385a73
Marcel Hering <rygos@icloud.com>
2017-03-06 17:08:25 +0100

    Temporär die Logoauslieferung deaktiviert.

--- resources/views/_partials/header.blade.php
+++ resources/views/_partials/header.blade.php
@@ -1,7 +1,8 @@
 <header>
     <h1>{{ trans('app.title') }}</h1>
     <div id='logo'>
-        <a href="/"><img height="100px" src="{{ url('logo/'.$logo->filename) }}" alt="Logo: {{ $logo->title }}"/></a>
+        {{-- <a href="/"><img height="100px" src="{{ url('logo/'.$logo->filename) }}" alt="Logo: {{ $logo->title }}"/></a> --}}
+        <a href="/">LOGO</a>
         <p>logo '{{ $logo->title }}' by <a href='{{ url('users', $logo->id) }}' class='user'>{{ $logo->name }}</a> :: {{ config('app.name') }} is brought to you with love.</p>
     </div>
 </header>
