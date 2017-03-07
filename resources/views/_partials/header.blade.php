ee4df6af504b894ee05d5622ccec4419bd94f042
Marcel Hering <rygos@icloud.com>
2017-03-07 09:42:49 +0100

    Header angepasst

--- resources/views/_partials/header.blade.php
+++ resources/views/_partials/header.blade.php
@@ -4,7 +4,7 @@
         @php
             $fname = str_replace('storage/logos/', '',$logo->filename)
         @endphp
-        <a href="/"><img height="100px" src="{{ url('logo/'.$logo->filename) }}" alt="Logo: {{ $logo->title }}"/></a>
+        <a href="/"><img height="100px" src="{{ url('logo/'.$fname) }}" alt="Logo: {{ $logo->title }}"/></a>
         <p>logo '{{ $logo->title }}' by <a href='{{ url('users', $logo->id) }}' class='user'>{{ $logo->name }}</a> :: {{ config('app.name') }} is brought to you with love.</p>
     </div>
 </header>
