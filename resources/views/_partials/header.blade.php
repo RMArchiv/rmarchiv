66b5bde5689caab8a1ff08ab2ac4b48a7c636509
Marcel Hering <rygos@icloud.com>
2017-03-07 09:40:51 +0100

    Header angepasst

--- resources/views/_partials/header.blade.php
+++ resources/views/_partials/header.blade.php
@@ -1,8 +1,7 @@
 <header>
     <h1>{{ trans('app.title') }}</h1>
     <div id='logo'>
-        {{-- <a href="/"><img height="100px" src="{{ url('logo/'.$logo->filename) }}" alt="Logo: {{ $logo->title }}"/></a> --}}
-        <a href="/">LOGO</a>
+        <a href="/"><img height="100px" src="{{ url('logo/'.$logo->filename) }}" alt="Logo: {{ $logo->title }}"/></a>
         <p>logo '{{ $logo->title }}' by <a href='{{ url('users', $logo->id) }}' class='user'>{{ $logo->name }}</a> :: {{ config('app.name') }} is brought to you with love.</p>
     </div>
 </header>
