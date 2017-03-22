f75b9e9c37ba2ddf1b35acf77927ba698f99b6ec
Marcel Hering <broxxer@broxxer.de>
2017-03-22 10:41:42 +0100

    inline game box test

--- resources/views/_partials/inline_gamebox.blade.php
+++ resources/views/_partials/inline_gamebox.blade.php
@@ -1,21 +1,23 @@
-<div class="inlinebox_game">
-    <div class="game_screen">
-        <img src='{{ route('screenshot.show', [$game->id, 1]) }}' alt='Titelbild' title='Titelbild'/>
-    </div>
-    <div class="game_info">
-        <div class="game_title">
+<table width="520px">
+    <tr>
+        <td colspan="2">
             <a href="{{ route('games.show', $game->id) }}"><big>{{ $game->title }}</big>@if($game->subtitle) :: {{ $game->subtitle }}@endif</a>
-        </div>
-        <div class="game_dev">
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <img src='{{ route('screenshot.show', [$game->id, 1]) }}' alt='Titelbild' title='Titelbild'/>
+        </td>
+        <td>
+            Entwickler:
             @foreach($game->developers as $dev)
                 <a href="{{ url('developer',$dev->developer_id) }}">{{ $dev->developer->name }}</a>
                 @if($dev != $game->developers->last())
                     ::
                 @endif
             @endforeach
-        </div>
-        <div class="game_desc">
+            <br>
             {!! substr($game->desc_md, 0, 240).'...' !!}
-        </div>
-    </div>
-</div>
+        </td>
+    </tr>
+</table>
