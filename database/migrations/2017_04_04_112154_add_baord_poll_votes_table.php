be62c94e72296cef7feee8d9ed4b4eab7b265070
Marcel Hering <broxxer@broxxer.de>
2017-04-05 08:29:03 +0200

    Migrationen für Umfragen hinzugefügt.

---
+++ database/migrations/2017_04_04_112154_add_baord_poll_votes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddBaordPollVotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('board_poll_votes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('poll_id');
+            $table->integer('answer_id');
+            $table->integer('user_id');
+            $table->timestamps();
+
+            $table->index('poll_id');
+            $table->index('answer_id');
+            $table->index('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('board_poll_votes');
+    }
+}
