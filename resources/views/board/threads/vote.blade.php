ce74ef18169368984ebf5e14b3813f8ac016862b
Marcel Hering <broxxer@broxxer.de>
2017-04-10 11:28:54 +0200

    Anzeige der Umfrage View

--- resources/views/board/threads/vote.blade.php
+++ resources/views/board/threads/vote.blade.php
@@ -2,8 +2,33 @@
 @section('content')
     @if(Auth::check())
         @if(Auth::user()->id == $thread->user_id or Auth::user()->can('mod-threads'))
-            <div class="rmarchivtbl">
+            <div id="content">
+                <form action="{{ route('board.vote.store', [$thread->thread_id]) }}" method="post" enctype="multipart/form-data">
+                    {{ csrf_field() }}
 
+                    <div class="rmarchivtbl" id="rmarchivbox_submitnews" style="width: 60%">
+                        <h2>umfrage erstellen</h2>
+
+                        @if (count($errors) > 0))
+                        <div class="rmarchivtbl errorbox">
+                            <h2>{{ trans('app.news.add.error.title') }}</h2>
+                            <div class="content">
+                                @foreach ($errors->all() as $error)
+                                    <strong>{{ $error }}</strong>
+                                @endforeach
+                            </div>
+                        </div>
+                        @endif
+
+                        <div class="content">
+                            <input type="hidden" name="thread_id" id="thread_id" value="{{ $thread->thread_id }}">
+
+                        </div>
+                        <div class="foot">
+                            <input type="submit" value="speichern">
+                        </div>
+                    </div>
+                </form>
             </div>
         @else
             <div id="content">
