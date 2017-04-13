5b0d6f037a60f1a2534c6c6e0c32a915257385ff
Marcel Hering <broxxer@broxxer.de>
2017-04-13 13:48:35 +0200

    translation manager

--- resources/views/translate/show.blade.php
+++ resources/views/translate/show.blade.php
@@ -3,14 +3,16 @@
     <div id="content">
         <table id='pouetbox_userlist' class='boxtable pagedtable'>
             <thead>
-            <th>part</th>
+            <th>group</th>
+            <th>item</th>
             <th>basis ({{$loc1}})</th>
             <th>ziel ({{$loc2}})</th>
             </thead>
             @foreach($list as $l)
                 <tr>
-                    <td>{{ dd($l) }}</td>
-                    <td></td>
+                    <td>{{ $l->group }}</td>
+                    <td>{{ $l->item }}</td>
+                    <td>{{ $l->text }}</td>
                     <td></td>
                 </tr>
             @endforeach
