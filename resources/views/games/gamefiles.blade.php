58f0b576a98e5359c603e841f7b4326fffd84018
Marcel Hering <rygos@icloud.com>
2017-04-18 21:13:15 +0200

    typo

--- resources/views/games/gamefiles.blade.php
+++ resources/views/games/gamefiles.blade.php
@@ -59,7 +59,7 @@
                             :: [<a href="{{ route('gamefiles.edit', [$gf->gameid, $gf->fileid]) }}">{{trans('games.gamefiles.edit')}}</a>]
                         @if(Auth::user()->settings->is_admin)
                             :: [<a href="{{ route("gamefiles.delete", [$gameid, $gf->fileid]) }}">{{trans('games.gamefiles.delete')}}</a>]
-                            :: <a href="{{ route('player.run', [$gf->gameid, $gf->id]) }}">play</a>
+                            :: <a href="{{ route('player.run', [$gf->gameid, $gf->fileid]) }}">play</a>
                         @endif
                         </td>
                     @endif
