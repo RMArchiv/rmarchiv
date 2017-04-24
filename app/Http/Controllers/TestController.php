85b5f4ab5474f91801dcb1cf1d2e167ed773b4a9
Marcel Hering <broxxer@broxxer.de>
2017-04-24 15:14:07 +0200

    player: testen von rar entpacken

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -12,6 +12,7 @@ class TestController extends Controller
     public function index()
     {
         //'token' => 'MjgzOTU3MzExMDYyNTQwMjg4.C48pjA._BS4vqj6a-DFIFOntDAkSx1A-GM',
+        return phpinfo();
     }
 
     public function webhook()
