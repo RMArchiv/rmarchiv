07228e5da26dc7b0061b7863c72220dace5abfda
Marcel Hering <broxxer@broxxer.de>
2017-04-26 16:59:20 +0200

    player: rtp assets hinzugefügt

--- app/Console/Commands/PlayerRar2Zip.php
+++ app/Console/Commands/PlayerRar2Zip.php
@@ -59,10 +59,44 @@ class PlayerRar2Zip extends Command
             }
             closedir($handle);
             $zip->close();
+            $this->Delete($pathdest);
             echo $f->filename.PHP_EOL;
             echo "fertig";
 
             return;
         }
+    }
+
+    function Delete($path)
+    {
+        if (is_dir($path) === true)
+        {
+            $files = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($path), \RecursiveIteratorIterator::CHILD_FIRST);
+
+            foreach ($files as $file)
+            {
+                if (in_array($file->getBasename(), array('.', '..')) !== true)
+                {
+                    if ($file->isDir() === true)
+                    {
+                        rmdir($file->getPathName());
+                    }
+
+                    else if (($file->isFile() === true) || ($file->isLink() === true))
+                    {
+                        unlink($file->getPathname());
+                    }
+                }
+            }
+
+            return rmdir($path);
+        }
+
+        else if ((is_file($path) === true) || (is_link($path) === true))
+        {
+            return unlink($path);
+        }
+
+        return false;
     }
 }
