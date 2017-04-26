94c3e149e33a0524b5526766aa069d4594291ec2
Marcel Hering <broxxer@broxxer.de>
2017-04-26 09:33:53 +0200

    player: rtp assets hinzugefügt

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -38,9 +38,29 @@ class PlayerController extends Controller
         $res['charset\/chara2'] = 'rtp\/charset_chara2.png';
         $res['charset\/chara3'] = 'rtp\/charset_chara3.png';
         $res['charset\/chara4'] = 'rtp\/charset_chara4.png';
+        $res['charset\/object1'] = 'rtp\/charset_object1.png';
+        $res['charset\/object2'] = 'rtp\/charset_object2.png';
+        $res['charset\/object3'] = 'rtp\/charset_object3.png';
+        $res['charset\/object6'] = 'rtp\/charset_object6.png';
+        $res['charset\/object9'] = 'rtp\/charset_object9.png';
+        $res['charset\/objecta'] = 'rtp\/charset_objecta.png';
+        $res['charset\/objectb'] = 'rtp\/charset_objectb.png';
+        $res['charset\/objectc'] = 'rtp\/charset_objectc.png';
+        $res['charset\/objectd'] = 'rtp\/charset_objectd.png';
+        $res['charset\/objecte'] = 'rtp\/charset_objecte.png';
+        $res['charset\/objectf'] = 'rtp\/charset_objectf.png';
+        $res['charset\/objectg'] = 'rtp\/charset_objectg.png';
+        $res['charset\/objecth'] = 'rtp\/charset_objecth.png';
+        $res['charset\/objecti'] = 'rtp\/charset_objecti.png';
+        $res['charset\/objectj'] = 'rtp\/charset_objectj.png';
+        $res['charset\/objectk'] = 'rtp\/charset_objectk.png';
+        $res['charset\/objectl'] = 'rtp\/charset_objectl.png';
 
         $res['chipset\/basis'] = 'rtp\/chipset_basis.png';
+        $res['chipset\/outline'] = 'rtp\/chipset_outline.png';
 
+        $res['music\/hero1'] = 'rtp\/music_hero1.mid';
+        $res['music\/hero2'] = 'rtp\/music_hero2.mid';
         $res['music\/opening1'] = 'rtp\/music_opening1.mid';
         $res['music\/opening2'] = 'rtp\/music_opening2.mid';
         $res['music\/opening3'] = 'rtp\/music_opening3.mid';
