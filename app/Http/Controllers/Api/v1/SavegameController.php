e36653ca6910ebd7ac99232e17301c9d576e9497
Marcel Hering <broxxer@broxxer.de>
2017-05-02 09:36:10 +0200

    Problem bei dortierung der Spieletabelle im entwicklerprofil.

---
+++ app/Http/Controllers/Api/v1/SavegameController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SavegameController extends Controller
+{
+    public function show($gamfileid){
+
+    }
+
+    public function create($gamefileid, $slotid){
+
+    }
+
+}
---
+++ app/Http/Controllers/SavegameController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SavegameController extends Controller
+{
+    public function index($gamefileid){
+
+    }
+
+    public function show($gamefileid, $slotid){
+
+    }
+
+}
