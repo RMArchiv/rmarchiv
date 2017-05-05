0824654d34d39221baafb0217faf07a53fb4d138
Marcel Hering <broxxer@broxxer.de>
2017-05-05 14:29:09 +0200

    DeveloperIndex: fertig

--- resources/views/developer/index.blade.php
+++ resources/views/developer/index.blade.php
@@ -1,51 +1,66 @@
 @extends('layouts.app')
 @section('pagetitle', trans('developer.index.title'))
 @section('content')
-    <div id="content">
-        <div class='rmarchivtbl' id='rmarchivbox_grouplist'>
-            <table id="rmarchiv_creatortable" class='boxtable'>
-                <thead>
-                <tr class='sortable'>
-                    <th>
-                        @if($orderby == 'devname')
-                            @if($direction == 'asc')
-                                <a class="activated" href="{{ route('developer.index.sorted', ['devname', 'desc']) }}">{{ trans('developer.index.developer') }}</a>
-                            @else
-                                <a class="activated reverse" href="{{ route('developer.index.sorted', ['devname', 'asc']) }}">{{ trans('developer.index.developer') }}</a>
-                            @endif
-                        @else
-                            <a class="" href="{{ route('developer.index.sorted', ['devname', 'asc']) }}">{{ trans('developer.index.developer') }}</a>
-                        @endif
-                    </th>
-                    <th>
-                        @if($orderby == 'gamecount')
-                            @if($direction == 'asc')
-                                <a class="activated" href="{{ route('developer.index.sorted', ['gamecount', 'desc']) }}">{{ trans('developer.index.games') }}</a>
-                            @else
-                                <a class="activated reverse" href="{{ route('developer.index.sorted', ['gamecount', 'asc']) }}">{{ trans('developer.index.games') }}</a>
-                            @endif
-                        @else
-                            <a class="" href="{{ route('developer.index.sorted', ['gamecount', 'asc']) }}">{{ trans('developer.index.games') }}</a>
-                        @endif
-                    </th>
-                </tr>
-                </thead>
-                @foreach($developer as $dev)
-                    @if($dev->gamecount <> 0 and $dev->devname <> '')
-                <tr>
-                    <td class='groupname'>
-                        <a href='{{ url('developer',$dev->devid) }}'>{{ $dev->devname }}</a>
-                    </td>
-                    <td>
-                        {{ $dev->gamecount }}
-                    </td>
-                </tr>
+    <div class="container">
+        <div class="row">
+            <div class="page-header">
+                <h1>{{trans('developer.index.title')}}</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    @if($developer instanceof \Illuminate\Pagination\LengthAwarePaginator )
+                        {{ $developer->links('vendor.pagination.bootstrap-4') }}
                     @endif
-                @endforeach
-                @if($developer instanceof \Illuminate\Pagination\LengthAwarePaginator )
-                    {{ $developer->links('vendor.pagination.gamelist') }}
-                @endif
-            </table>
+                </div>
+                <table class='table table-striped'>
+                    <thead>
+                    <tr class="active">
+                        <th>
+                            @if($orderby == 'devname')
+                                @if($direction == 'asc')
+                                    <a class="activated" href="{{ route('developer.index.sorted', ['devname', 'desc']) }}">{{ trans('developer.index.developer') }}</a>
+                                @else
+                                    <a class="activated reverse" href="{{ route('developer.index.sorted', ['devname', 'asc']) }}">{{ trans('developer.index.developer') }}</a>
+                                @endif
+                            @else
+                                <a class="" href="{{ route('developer.index.sorted', ['devname', 'asc']) }}">{{ trans('developer.index.developer') }}</a>
+                            @endif
+                        </th>
+                        <th>
+                            @if($orderby == 'gamecount')
+                                @if($direction == 'asc')
+                                    <a class="activated" href="{{ route('developer.index.sorted', ['gamecount', 'desc']) }}">{{ trans('developer.index.games') }}</a>
+                                @else
+                                    <a class="activated reverse" href="{{ route('developer.index.sorted', ['gamecount', 'asc']) }}">{{ trans('developer.index.games') }}</a>
+                                @endif
+                            @else
+                                <a class="" href="{{ route('developer.index.sorted', ['gamecount', 'asc']) }}">{{ trans('developer.index.games') }}</a>
+                            @endif
+                        </th>
+                    </tr>
+                    </thead>
+                    @foreach($developer as $dev)
+                        @if($dev->gamecount <> 0 and $dev->devname <> '')
+                            <tr>
+                                <td class='groupname'>
+                                    <a href='{{ url('developer',$dev->devid) }}'>{{ $dev->devname }}</a>
+                                </td>
+                                <td>
+                                    <span class="badge">{{ $dev->gamecount }}</span>
+                                </td>
+                            </tr>
+                        @endif
+                    @endforeach
+
+                </table>
+                <div class="panel-footer">
+                    @if($developer instanceof \Illuminate\Pagination\LengthAwarePaginator )
+                        {{ $developer->links('vendor.pagination.bootstrap-4') }}
+                    @endif
+                </div>
+            </div>
         </div>
     </div>
 @endsection
--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -1,84 +1,80 @@
 <?php
 
 // Home
-Breadcrumbs::register('home', function($breadcrumbs)
-{
+Breadcrumbs::register('home', function ($breadcrumbs) {
     $breadcrumbs->push('home', action('IndexController@index'));
 });
 
 // Home > Games
-Breadcrumbs::register('games', function($breadcrumbs)
-{
+Breadcrumbs::register('games', function ($breadcrumbs) {
     $breadcrumbs->parent('home');
     $breadcrumbs->push(trans('games.title'), url('games'));
 });
 
 // Home > Games > [game]
-Breadcrumbs::register('game', function($breadcrumbs, $game)
-{
+Breadcrumbs::register('game', function ($breadcrumbs, $game) {
     $breadcrumbs->parent('games');
-    if($game->subtitle){
-        $breadcrumbs->push($game->title.' - '.$game->subtitle, action('GameController@show', $game->id));
-    }else{
+    if ($game->subtitle) {
+        $breadcrumbs->push($game->title . ' - ' . $game->subtitle, action('GameController@show', $game->id));
+    } else {
         $breadcrumbs->push($game->title, action('GameController@show', $game->id));
     }
 });
 
 // Home > Games > erstellen
-Breadcrumbs::register('game-add', function($breadcrumbs){
+Breadcrumbs::register('game-add', function ($breadcrumbs) {
     $breadcrumbs->parent('games');
     $breadcrumbs->push('erstellen', action('GameController@create'));
 });
 
 // Home > Games > [game] > edit
-Breadcrumbs::register('game-edit', function($breadcrumbs, $game)
-{
+Breadcrumbs::register('game-edit', function ($breadcrumbs, $game) {
     $breadcrumbs->parent('game', $game);
-    if($game->subtitle){
+    if ($game->subtitle) {
         $breadcrumbs->push('bearbeiten', action('GameController@edit', $game->id));
-    }else{
+    } else {
         $breadcrumbs->push('bearbeiten', action('GameController@edit', $game->id));
     }
 });
 
 // Home > FAQ
-Breadcrumbs::register('faq', function($breadcrumbs)
-{
+Breadcrumbs::register('faq', function ($breadcrumbs) {
     $breadcrumbs->parent('home');
     $breadcrumbs->push(trans('faq.title'), action('FaqController@index'));
 });
 
 // Home > Users
-Breadcrumbs::register('users', function($breadcrumbs)
-{
+Breadcrumbs::register('users', function ($breadcrumbs) {
     $breadcrumbs->parent('home');
     $breadcrumbs->push(trans('user.title'), action('UserController@index'));
 });
 
 // Home > Users > Online
-Breadcrumbs::register('online', function($breadcrumbs)
-{
+Breadcrumbs::register('online', function ($breadcrumbs) {
     $breadcrumbs->parent('users');
     $breadcrumbs->push('online', action('UserController@users_online'));
 });
 
 // Home > Board
-Breadcrumbs::register('forums', function($breadcrumbs)
-{
+Breadcrumbs::register('forums', function ($breadcrumbs) {
     $breadcrumbs->parent('home');
     $breadcrumbs->push('forum', action('BoardController@index'));
 });
 
 // Home > Board > [Forum]
-Breadcrumbs::register('forum', function($breadcrumbs, $boardcat)
-{
+Breadcrumbs::register('forum', function ($breadcrumbs, $boardcat) {
     $breadcrumbs->parent('forums');
     $breadcrumbs->push($boardcat->title, action('BoardController@show_cat', $boardcat->id));
 });
 
 // Home > Board > [Forum] -> [Thread]
-Breadcrumbs::register('thread', function($breadcrumbs, $boardcat, $thread)
-{
+Breadcrumbs::register('thread', function ($breadcrumbs, $boardcat, $thread) {
     $breadcrumbs->parent('forum', $boardcat);
     $breadcrumbs->push($thread->title, action('BoardController@show_thread', $thread->id));
+});
+
+// Home > Developers
+Breadcrumbs::register('developers', function ($breadcrumbs) {
+    $breadcrumbs->parent('home');
+    $breadcrumbs->push('entwickler', action('DeveloperController@index'));
 });
