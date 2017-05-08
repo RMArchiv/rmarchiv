d164646c5d7019987a0e524d8274b88e03108660
Marcel Hering <broxxer@broxxer.de>
2017-05-08 09:03:07 +0200

    Impressum: Fertig, TypeAhead: Fertig.

--- resources/views/games/create.blade.php
+++ resources/views/games/create.blade.php
@@ -124,8 +124,8 @@
                             </div>
                             <div class="form-group">
                                 <div class="col-lg-10 col-lg-offset-2">
-                                    <button type="reset" class="btn btn-default">Cancel</button>
-                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                    <button type="reset" class="btn btn-default">abbrechen</button>
+                                    <button type="submit" class="btn btn-primary">{{ trans('games.create.send') }}</button>
                                 </div>
                             </div>
                         </fieldset>
