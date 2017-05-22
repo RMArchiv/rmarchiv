8b4c242e91e5a677e71c6fa67d0eecf79dd3f8d6
Marcel Hering <broxxer@broxxer.de>
2017-05-22 15:11:52 +0200

    Fix InlineGamebox

--- resources/views/_partials/inline_gamebox.blade.php
+++ resources/views/_partials/inline_gamebox.blade.php
@@ -1,23 +1,70 @@
-<table class="inline_game_box">
-    <tr>
-        <td colspan="2">
-            <a href="{{ route('games.show', $game->id) }}"><big>{{ $game->title }}</big>@if($game->subtitle) :: {{ $game->subtitle }}@endif</a>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <img src='{{ route('screenshot.show', [$game->id, 1]) }}' alt='{{ trans('_partials.inline_gamebox.titlescreen') }}' title='{{ trans('_partials.inline_gamebox.titlescreen') }}' style="width: 160px;"/>
-        </td>
-        <td>
-            {{ trans('_partials.inline_gamebox.developer') }}:
-            @foreach($game->developers as $dev)
-                <a href="{{ url('developer',$dev->developer_id) }}">{{ $dev->developer->name }}</a>
-                @if($dev != $game->developers->last())
-                    ::
-                @endif
-            @endforeach
-            <br>
-            {!! substr($game->desc_md, 0, 320).'...' !!}
-        </td>
-    </tr>
-</table>
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <a href='{{ url('games', $game->id) }}'>
+            @if($game->gamefiles->count() > 0)
+                <span class='typeiconlist'>
+                        <span class='typei type_{{ $game->gamefiles->first()->gamefiletype->short }}'
+                              title='{{ $game->gamefiles->first()->gamefiletype->title }}'>{{ $game->gamefiles->first()->gamefiletype->title }}</span>
+                    </span>
+            @endif
+            <span class="platformiconlist">
+                    <a href="{{ route('maker.show', $game->maker->id) }}">
+                        <span class="typei type_{{ $game->maker->short }}" title="{{ $game->maker->title }}">
+                            {{ $game->maker->title }}
+                        </span>
+                    </a>
+            {{ $game->title }}
+            @if($game->subtitle)
+                <small> - {{ $game->subtitle }}</small>
+            @endif
+            <span><img src="/assets/lng/16/{{ strtoupper($game->language->short) }}.png"
+                       title="{{ $game->language->name }}"></span>
+        </a>
+    </div>
+    <ul class="list-group">
+        <li class="list-group-item media" style="margin-top: 0px;">
+            <a class="pull-right" href="{{ url('games', $game->id) }}"><span class="badge">{{ $game->comments }}</span></a>
+            <a class="pull-left" href="{{ url('games', $game->id) }}"><img width="100px" class="img-responsive img-rounded" src='{{ route('screenshot.show', [$game->id, 1]) }}' alt='{{ trans('games.show.titlescreen') }}' title='{{ trans('games.show.titlescreen') }}'/></a>
+            <div class="thread-info">
+                <div class="media-heading">
+                    {!! substr($game->desc_md, 0, 320).'...' !!}
+
+                </span>
+                    @if($game->cdcs->count() > 0)
+                        <div class="cdcstack">
+                            <img src="/assets/cdc.png" title="{{ trans('app.cdc.title') }}" alt="{{ trans('app.cdc.title') }}">
+                        </div>
+                    @endif
+                </div>
+                <div class="media-body" style="font-size: 12px;">
+                    {!! \App\Helpers\DatabaseHelper::getDevelopersUrlList($game->id) !!}<br>
+                    release date:
+                    @if(\Carbon\Carbon::parse($game->release_date)->year != -1 )
+                        {{ $game->release_date }}
+                    @else
+                        {{ \Carbon\Carbon::parse(\App\Helpers\DatabaseHelper::getReleaseDateFromGameId($game->id))->toDateString() }}
+                    @endif
+                    <span> • </span>
+                    hinzugefügt {{ \Carbon\Carbon::parse($game->created_at)->diffForHumans() }}
+                    <span> • </span>
+                    <img src='/assets/rate_up.gif' alt='{{ trans('app.games.table.rate_up') }}'/> {{ $game->voteup or 0 }} -
+                    <img src='/assets/rate_down.gif' alt='{{ trans('app.games.table.rate_down') }}'/> {{ $game->votedown or 0 }}
+                    <span> • </span>
+                    AVG: {{ number_format(floatval($game->avg), 2) }}&nbsp;
+                    @if($game->avg > 0)
+                        <img src='/assets/rate_up.gif' alt='{{ trans('app.games.table.rate_up') }}'/>
+                    @elseif($game->avg == 0)
+                        <img src='/assets/rate_neut.gif' alt='{{ trans('app.games.table.rate_neut') }}'/>
+                    @elseif($game->avg < 0)
+                        <img src='/assets/rate_down.gif' alt='{{ trans('app.games.table.rate_down') }}'/>
+                    @endif
+                    <div class="pull-right">
+                        @foreach($game->tags as $tag)
+                            <a href="{{ action('TaggingController@showGames', [$tag->tag_id]) }}"><span class="label label-primary">{{ $tag->tag->title }}</span></a>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </li>
+    </ul>
+</div>
