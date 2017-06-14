e825af3dcdcb44580a528f1e9f107321c1b4779e
Marcel Hering <broxxer@broxxer.de>
2017-06-14 10:58:40 +0200

    Read More Button für Spielebeschreibungen - fix #221

--- resources/views/games/show.blade.php
+++ resources/views/games/show.blade.php
@@ -270,7 +270,7 @@
                             <div class="col-md-12">
                                 <div class="panel panel-default">
                                     <div class="panel-heading">{{ trans('games.show.gamedescription') }}</div>
-                                    <div class="panel-body">
+                                    <div class="panel-body readmore">
                                         {!! $game->desc_html !!}
                                     </div>
                                 </div>
@@ -531,4 +531,42 @@
     @else
         <h1>{{ trans('games.show.no_id') }}</h1>
     @endif
+
+    <script>
+        $(function () { /* to make sure the script runs after page load */
+
+            $('.readmore').each(function (event) { /* select all divs with the item class */
+
+                var max_length = 512;
+                /* set the max content length before a read more link will be added */
+
+                if ($(this).html().length > max_length) { /* check for content length */
+
+                    var short_content = $(this).html().substr(0, max_length);
+                    /* split the content in two parts */
+                    var long_content = $(this).html().substr(max_length);
+
+                    $(this).html(short_content +
+                        '<a href="#" class="read_more"><br/>Read More</a>' +
+                        '<div class="more_text" style="display:none;">' + long_content + '</div>');
+                    /* Alter the html to allow the read more functionality */
+
+                    $(this).find('a.read_more').click(function (event) { /* find the a.read_more element within the new html and bind the following code to it */
+
+                        event.preventDefault();
+                        /* prevent the a from changing the url */
+                        $(this).hide();
+                        /* hide the read more button */
+                        $(this).parents('.readmore').find('.more_text').show();
+                        /* show the .more_text span */
+
+                    });
+
+                }
+
+            });
+
+
+        });
+    </script>
 @endsection
