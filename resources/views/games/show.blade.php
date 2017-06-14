82264b6090de591893e02cf15a845c31321e2dfa
Marcel Hering <broxxer@broxxer.de>
2017-06-14 15:22:15 +0200

    suggestions

--- resources/views/games/show.blade.php
+++ resources/views/games/show.blade.php
@@ -537,7 +537,7 @@
 
             $('.readmore').each(function (event) { /* select all divs with the item class */
 
-                var max_length = 512;
+                var max_length = 1024;
                 /* set the max content length before a read more link will be added */
 
                 if ($(this).html().length > max_length) { /* check for content length */
@@ -546,9 +546,10 @@
                     /* split the content in two parts */
                     var long_content = $(this).html().substr(max_length);
 
-                    $(this).html(short_content +
-                        '<a href="#" class="read_more"><br/>Read More</a>' +
-                        '<div class="more_text" style="display:none;">' + long_content + '</div>');
+                    $(this).html(
+                        '<div class="short_text"' + short_content + '</div>' +
+                        '<a href="#" class="read_more"><br/>mehr lesen...</a>' +
+                        '<div class="more_text" style="display:none;">' + long_content + '<a href="#" class="read_less"><br/>weniger lesen...</a></div>');
                     /* Alter the html to allow the read more functionality */
 
                     $(this).find('a.read_more').click(function (event) { /* find the a.read_more element within the new html and bind the following code to it */
@@ -559,6 +560,18 @@
                         /* hide the read more button */
                         $(this).parents('.readmore').find('.more_text').show();
                         /* show the .more_text span */
+                        $(this).parents('.readmore').find('.short_text').hide();
+
+                    });
+                    $(this).find('a.read_less').click(function (event) { /* find the a.read_more element within the new html and bind the following code to it */
+
+                        event.preventDefault();
+                        /* prevent the a from changing the url */
+                        $(this).hide();
+                        /* hide the read more button */
+                        $(this).parents('.readmore').find('.short_text').show();
+                        /* show the .more_text span */
+                        $(this).parents('.readmore').find('.more_text').hide();
 
                     });
 
