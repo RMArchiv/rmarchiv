162dda0ccb010e859fe400b7365fdebe05b5aa6b
Marcel Hering <broxxer@broxxer.de>
2017-06-19 11:16:18 +0200

    session to database

--- resources/views/_partials/footer.blade.php
+++ resources/views/_partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="text-center">
     <ul class="list-unstyled">
         <li>
-            <a href="/">{{ config('app.name') }}</a> {{ config('app.version') }} &copy; 2016-{{ date('Y', time()) }} by rmarchiv.de Team
+            <a href="/">{{ config('app.name') }}</a> {{ config('app.version') }}.{{ App::getLocale() }} &copy; 2016-{{ date('Y', time()) }} by rmarchiv.de Team
         </li>
         <li>
             {{ trans('_partials.footer.feedback') }} <a href="mailto:webmaster@rmarchiv.de">webmaster@rmarchiv.de</a>
