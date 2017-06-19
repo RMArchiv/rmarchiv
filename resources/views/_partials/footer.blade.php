23feb8e604bf37503a3892166dbe02d2df13dcd5
Marcel Hering <broxxer@broxxer.de>
2017-06-19 10:55:25 +0200

    show language in footer + change session cookie

--- resources/views/_partials/footer.blade.php
+++ resources/views/_partials/footer.blade.php
@@ -1,7 +1,11 @@
 <footer class="text-center">
     <ul class="list-unstyled">
         <li>
-            <a href="/">{{ config('app.name') }}</a> {{ config('app.version') }}.{{ \App\Helpers\MiscHelper::get_current_git_commit() }} &copy; 2016-{{ date('Y', time()) }} by rmarchiv.de Team
+            @php
+                $url_array = explode('.', parse_url($request->url(), PHP_URL_HOST));
+                $subdomain = $url_array[0];
+            @endphp
+            <a href="/">{{ config('app.name') }}</a> {{ config('app.version') }}.{{ $subdomain }} &copy; 2016-{{ date('Y', time()) }} by rmarchiv.de Team
         </li>
         <li>
             {{ trans('_partials.footer.feedback') }} <a href="mailto:webmaster@rmarchiv.de">webmaster@rmarchiv.de</a>
