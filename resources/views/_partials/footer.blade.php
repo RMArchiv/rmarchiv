c487b42ed75801f2e2850bee821df98a5d6756ac
Marcel Hering <broxxer@broxxer.de>
2017-06-19 11:13:46 +0200

    session to database

--- resources/views/_partials/footer.blade.php
+++ resources/views/_partials/footer.blade.php
@@ -1,11 +1,7 @@
 <footer class="text-center">
     <ul class="list-unstyled">
         <li>
-            @php
-                $url_array = explode('.', parse_url($request->url(), PHP_URL_HOST));
-                $subdomain = $url_array[0];
-            @endphp
-            <a href="/">{{ config('app.name') }}</a> {{ config('app.version') }}.{{ $subdomain }} &copy; 2016-{{ date('Y', time()) }} by rmarchiv.de Team
+            <a href="/">{{ config('app.name') }}</a> {{ config('app.version') }} &copy; 2016-{{ date('Y', time()) }} by rmarchiv.de Team
         </li>
         <li>
             {{ trans('_partials.footer.feedback') }} <a href="mailto:webmaster@rmarchiv.de">webmaster@rmarchiv.de</a>
