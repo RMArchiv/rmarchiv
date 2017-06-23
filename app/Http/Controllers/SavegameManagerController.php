67659c8e507ec9901707306a4a05b1dc7da854c2
Marcel Hering <broxxer@broxxer.de>
2017-06-23 13:49:31 +0200

    debug test

--- app/Http/Controllers/SavegameManagerController.php
+++ app/Http/Controllers/SavegameManagerController.php
@@ -140,7 +140,9 @@ class SavegameManagerController extends Controller
                 'gamefile_id' => 'required',
             ]);
 
-            $data = file_get_contents($request->file('file')[0]->getRealPath());
+            dd($request->file('file'));
+
+            $data = file_get_contents($request->file('file')->getRealPath());
 
             if(PlayerHelper::getSavegameValidation($data) == true){
                 $check = GamesSavegame::whereGamefileId($request->get('gamefile_id'))
