af71a1c8a66772256f34e48b977f8387572c4b5b
ryg <rygos@icloud.com>
2017-08-21 13:01:48 +0200

    fix #242

---
+++ resources/views/board/_partials/overview_cat_row.blade.php
@@ -0,0 +1,23 @@
+<li class="list-group-item media" style="margin-top: 0px;">
+    @php $cat = $cats->where('id', '=', $id)->first() @endphp
+    <a class="pull-right" href="{{ route('board.cat.show', $cat->id) }}"><span class="badge">{{ $count = \App\Models\BoardPost::whereCatId($cat->id)->count() }}</span></a>
+
+    @if($count != 0)
+        <a class="pull-left" href="{{ url('users', $cat->last_user->id) }}"><img class="media-object img-rounded" width="42px" src="//{{ config('app.avatar_path') }}?size=42&gender=male&id={{ $cat->last_user->id }}" alt="{{ $cat->last_user->name }}"></a>
+    @endif
+
+    <div class="thread-info">
+        <div class="media-heading">
+            <a href="{{ route('board.cat.show', $cat->id) }}">{{ $cat->title }}</a>
+        </div>
+        <div class="media-body" style="font-size: 12px;">
+            @if($count != 0)
+            {{ trans('app.last_reply') }}
+                <time datetime='{{ $cat->last_created_at }}' title='{{ $cat->last_created_at }}'>{{ \Carbon\Carbon::parse($cat->last_created_at)->diffForHumans() }}</time> {{ trans('app.by') }}
+                <a href='{{ url('users', $cat->last_user_id) }}' class='usera' title="{{ $cat->last_user->name }}">
+                    <img width="16px" class="img-rounded" src='//{{ config('app.avatar_path') }}?size=16&gender=male&id={{ $cat->last_user->id }}' alt="{{ $cat->last_user->name }}"/>
+                </a> <a href='{{ url('users', $cat->last_user->id) }}' class='user'>{{ $cat->last_user->name }}</a>
+            @endif
+        </div>
+    </div>
+</li>
--- resources/views/board/index.blade.php
+++ resources/views/board/index.blade.php
@@ -8,46 +8,42 @@
                 {!! Breadcrumbs::render('forums') !!}
             </div>
         </div>
-        @foreach($cats as $cat)
         <div class="row">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <h4><a href="{{ url('board/cat', $cat->id) }}">{{ $cat->title }}</a></h4>
+                    Organisatorisches
                 </div>
                 <ul class="list-group">
-                    @foreach($cat->threads->sortByDesc('last_created_at')->take(15) as $thread)
-                        <li class="list-group-item media" style="margin-top: 0px;">
-                            <a class="pull-right" href="{{ route('board.thread.show', $thread->id) }}"><span class="badge">{{ $thread->posts->count() }}</span></a>
-                            <a class="pull-left" href="{{ url('users', $thread->user->id) }}"><img class="media-object img-rounded" width="42px" src="//{{ config('app.avatar_path') }}?size=42&gender=male&id={{ $thread->user->id }}" alt="{{ $thread->user->name }}"></a>
-                            <div class="thread-info">
-                                <div class="media-heading">
-                                    @if($thread->closed == 1)
-                                        <i class="fa fa-lock text-danger"></i>
-                                    @endif
-                                    @if(\App\Models\BoardPoll::whereThreadId($thread->id)->count() != 0)
-                                            <i class="fa fa-signal fa-rotate-270"></i>
-                                    @endif
-                                    <a href="{{ route('board.thread.show', $thread->id) }}">{{ $thread->title }}</a>
-                                </div>
-                                <div class="media-body" style="font-size: 12px;">
-                                    <a href="{{ route('board.cat.show', $cat->id) }}"><span @if(\App\Helpers\DatabaseHelper::isThreadUnread($thread->id) === true) style="font-weight: bold;" @endif>{{ $cat->title }}</span></a>
-                                    <span> • </span>
-                                    {{ trans('app.created_at') }}
-                                    <time datetime='{{ $thread->created_at }}' title='{{ $thread->created_at }}'>{{ \Carbon\Carbon::parse($thread->created_at)->diffForHumans() }}</time>
-                                    <span> • </span>
-                                    {{ trans('app.last_reply') }}
-                                    <time datetime='{{ $thread->last_created_at }}' title='{{ $thread->last_created_at }}'>{{ \Carbon\Carbon::parse($thread->last_created_at)->diffForHumans() }}</time> {{ trans('app.by') }}
-                                    <a href='{{ url('users', $thread->last_user->id) }}' class='usera' title="{{ $thread->last_user->name }}">
-                                        <img width="16px" class="img-rounded" src='//{{ config('app.avatar_path') }}?size=16&gender=male&id={{ $thread->last_user->id }}' alt="{{ $thread->last_user->name }}"/>
-                                    </a> <a href='{{ url('users', $thread->last_user->id) }}' class='user'>{{ $thread->last_user->name }}</a>
-                                </div>
-                            </div>
-                        </li>
-                    @endforeach
+                    @include('board._partials.overview_cat_row', ['cats' => $cats, 'id' => 1])
                 </ul>
             </div>
         </div>
-        @endforeach
+        <div class="row">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    RPG Maker
+                </div>
+                <ul class="list-group">
+                    @include('board._partials.overview_cat_row', ['cats' => $cats, 'id' => 4])
+                    @include('board._partials.overview_cat_row', ['cats' => $cats, 'id' => 5])
+                    @include('board._partials.overview_cat_row', ['cats' => $cats, 'id' => 2])
+                    @include('board._partials.overview_cat_row', ['cats' => $cats, 'id' => 3])
+                    @include('board._partials.overview_cat_row', ['cats' => $cats, 'id' => 6])
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Community
+                </div>
+                <ul class="list-group">
+                    @include('board._partials.overview_cat_row', ['cats' => $cats, 'id' => 7])
+                    @include('board._partials.overview_cat_row', ['cats' => $cats, 'id' => 8])
+                </ul>
+            </div>
+        </div>
+
         <div class="row">
             @if(Auth::check())
                 <div class="panel panel-default">
