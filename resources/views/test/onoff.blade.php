00f7756780bf6c24d491e4abae8cfc73f105fdb0
ryg <rygos@icloud.com>
2017-10-04 15:01:29 +0200

    Avatar upload fix

--- resources/views/test/onoff.blade.php
+++ resources/views/test/onoff.blade.php
@@ -1,4 +1,4 @@
 <body>
-Script is: {{ $onoff }}<br>
-<a href="{{ action('TestController@on') }}">on</a> - <a href="{{ action('TestController@off') }}">off</a>
+<h1>Script is: {{ $onoff }}<br>
+    <a href="{{ action('TestController@on') }}">on</a> - <a href="{{ action('TestController@off') }}">off</a></h1>
 </body>
