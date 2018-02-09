0d515b97c0c2576b2b41f2d9f8d6449f07706b63
Marcel Hering <ryg@banncity.de>
2018-02-09 10:25:46 +0100

    bs4

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,39 +26,43 @@
         @endif
         <div class="row">
             <div class="well">
-                <div class="form-horizontal">
-                    <fieldset>
-                        <legend>
-                            {{ trans('app.login') }}
-                        </legend>
-                        <div class="form-group">
-                            <label for="email" class="col-lg-2 col-form-label">{{trans('app.email_address')}}</label>
-                            <div class="col-lg-10">
-                                <input type="text" class="form-control" id="email" name="email" placeholder="e.mail@mail.com">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="password" class="col-lg-2 col-form-label">{{trans('app.password')}}</label>
-                            <div class="col-lg-10">
-                                <input type="password" class="form-control" id="password" name="password">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember"> {{ trans('app.remember_login') }}
-                                    </label>
+                <div class="card">
+                    <div class="card-header">
+                        {{ trans('app.login') }}
+                    </div>
+                    <div class="card-body">
+                        <div class="form-horizontal">
+                            <fieldset>
+                                <div class="form-group">
+                                    <label for="email" class="col-lg-4 col-form-label">{{trans('app.email_address')}}</label>
+                                    <div class="col-lg-8">
+                                        <input type="text" class="form-control" id="email" name="email" placeholder="e.mail@mail.com">
+                                    </div>
                                 </div>
-                            </div>
+                                <div class="form-group">
+                                    <label for="password" class="col-lg-4 col-form-label">{{trans('app.password')}}</label>
+                                    <div class="col-lg-8">
+                                        <input type="password" class="form-control" id="password" name="password">
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox" name="remember"> {{ trans('app.remember_login') }}
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary">{{ trans('app.login') }}</button>
+                                    <div class="pull-right">
+                                        <a href="{{ url('/password/reset') }}">
+                                            {{ trans('app.password_reset') }}
+                                        </a>
+                                        -
+                                        <a href='{{ url('register') }}'>{{ trans('app.register') }}</a>
+                                    </div>
+                                </div>
+                            </fieldset>
                         </div>
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-primary">{{ trans('app.login') }}</button>
-                            <div class="pull-right">
-                                <a href="{{ url('/password/reset') }}">
-                                    {{ trans('app.password_reset') }}
-                                </a>
-                                -
-                                <a href='{{ url('register') }}'>{{ trans('app.register') }}</a>
-                            </div>
-                        </div>
-                    </fieldset>
+                    </div>
                 </div>
             </div>
         </div>
