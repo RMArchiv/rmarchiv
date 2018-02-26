1ac37188aad4b04820311a06ab0c0cdfef8b363b
Marcel Hering <ryg@banncity.de>
2018-02-26 11:54:25 +0100

    fix board views

--- resources/views/board/index.blade.php
+++ resources/views/board/index.blade.php
@@ -9,42 +9,49 @@
             </div>
         </div>
         <div class="row">
-            <div class="card">
-                <div class="card-header">
-                    Organisatorisches
+            <div class="col-md-12 mb-3">
+                <div class="card">
+                    <div class="card-header">
+                        Organisatorisches
+                    </div>
+                    <ul class="list-group">
+                        @include('board._partials.overview_cat_row', ['cats' => $cats, 'id' => 1])
+                    </ul>
                 </div>
-                <ul class="list-group">
-                    @include('board._partials.overview_cat_row', ['cats' => $cats, 'id' => 1])
-                </ul>
             </div>
         </div>
         <div class="row">
-            <div class="card">
-                <div class="card-header">
-                    RPG Maker
+            <div class="col-md-12 mb-3">
+                <div class="card">
+                    <div class="card-header">
+                        RPG Maker
+                    </div>
+                    <ul class="list-group">
+                        @include('board._partials.overview_cat_row', ['cats' => $cats, 'id' => 4])
+                        @include('board._partials.overview_cat_row', ['cats' => $cats, 'id' => 5])
+                        @include('board._partials.overview_cat_row', ['cats' => $cats, 'id' => 2])
+                        @include('board._partials.overview_cat_row', ['cats' => $cats, 'id' => 3])
+                        @include('board._partials.overview_cat_row', ['cats' => $cats, 'id' => 6])
+                    </ul>
                 </div>
-                <ul class="list-group">
-                    @include('board._partials.overview_cat_row', ['cats' => $cats, 'id' => 4])
-                    @include('board._partials.overview_cat_row', ['cats' => $cats, 'id' => 5])
-                    @include('board._partials.overview_cat_row', ['cats' => $cats, 'id' => 2])
-                    @include('board._partials.overview_cat_row', ['cats' => $cats, 'id' => 3])
-                    @include('board._partials.overview_cat_row', ['cats' => $cats, 'id' => 6])
-                </ul>
             </div>
         </div>
         <div class="row">
-            <div class="card">
-                <div class="card-header">
-                    Community
+            <div class="col-md-12 mb-3">
+                <div class="card">
+                    <div class="card-header">
+                        Community
+                    </div>
+                    <ul class="list-group">
+                        @include('board._partials.overview_cat_row', ['cats' => $cats, 'id' => 7])
+                        @include('board._partials.overview_cat_row', ['cats' => $cats, 'id' => 8])
+                    </ul>
                 </div>
-                <ul class="list-group">
-                    @include('board._partials.overview_cat_row', ['cats' => $cats, 'id' => 7])
-                    @include('board._partials.overview_cat_row', ['cats' => $cats, 'id' => 8])
-                </ul>
             </div>
         </div>
 
         <div class="row">
+            <div class="col-md-12 mb-3">
             @if(Auth::check())
                 <div class="card">
                     <div class="card-header">
@@ -80,6 +87,7 @@
                     </div>
                 </div>
             @endif
+            </div>
         </div>
     </div>
 @endsection
