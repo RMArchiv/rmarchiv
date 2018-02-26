41e5f5cb98ad15b96d189a1f3f3f15446afbe1c0
Marcel Hering <ryg@banncity.de>
2018-02-26 13:13:29 +0100

    fix news views

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -9,48 +9,50 @@
             </div>
         </div>
         <div class="row">
-            <div class="card">
-                <div class="card-header">
-                    {{ $news->links('vendor.pagination.bootstrap-4') }}
-                </div>
-                <ul class="list-group">
-                    @foreach($news as $item)
-                        @if($item->approved == 1)
-                            <li class="list-group-item media" style="margin-top: 0px;">
-                                <a class="pull-right" href="{{ route('news.show', $item->id) }}"><span class="badge">{{ $item->comments->count() }}</span></a>
-                                <a class="pull-left" href="{{ url('users', $item->user->id) }}"><img class="media-object img-rounded" width="42px" src="//{{ config('app.avatar_path') }}?size=42&gender=male&id={{ $item->user->id }}" alt="{{ $item->user->name }}"></a>
-                                <div class="thread-info">
-                                    <div class="media-heading">
-                                        <a href="{{ route('news.show', $item->id) }}">{{ $item->title }}</a>
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        {{ $news->links('vendor.pagination.bootstrap-4') }}
+                    </div>
+                    <ul class="list-group">
+                        @foreach($news as $item)
+                            @if($item->approved == 1)
+                                <li class="list-group-item media" style="margin-top: 0px;">
+                                    <a class="pull-right" href="{{ route('news.show', $item->id) }}"><span class="badge">{{ $item->comments->count() }}</span></a>
+                                    <a class="pull-left" href="{{ url('users', $item->user->id) }}"><img style="width: 48px;" class="mr-3" src="//{{ config('app.avatar_path') }}?size=42&gender=male&id={{ $item->user->id }}" alt="{{ $item->user->name }}"></a>
+                                    <div class="thread-info">
+                                        <div class="media-heading">
+                                            <a href="{{ route('news.show', $item->id) }}">{{ $item->title }}</a>
+                                        </div>
+                                        <div class="media-body" style="font-size: 12px;">
+                                            {{ trans('app.created_at') }}
+                                            <time datetime='{{ $item->created_at }}' title='{{ $item->created_at }}'>{{ \Carbon\Carbon::parse($item->created_at)->diffForHumans() }}</time>
+                                        </div>
                                     </div>
-                                    <div class="media-body" style="font-size: 12px;">
-                                        {{ trans('app.created_at') }}
-                                        <time datetime='{{ $item->created_at }}' title='{{ $item->created_at }}'>{{ \Carbon\Carbon::parse($item->created_at)->diffForHumans() }}</time>
+                                </li>
+                            @else
+                                @permission(('edit-news'))
+                                <li class="list-group-item active media" style="margin-top: 0px;">
+                                    <a class="pull-right" href="{{ route('news.show', $item->id) }}"><span class="badge">{{ $item->comments->count() }}</span></a>
+                                    <a class="pull-left" href="{{ url('users', $item->user->id) }}"><img class="media-object img-rounded" width="42px" src="//{{ config('app.avatar_path') }}?size=42&gender=male&id={{ $item->user->id }}" alt="{{ $item->user->name }}"></a>
+                                    <div class="thread-info">
+                                        <div class="media-heading">
+                                            <a href="{{ route('news.show', $item->id) }}">{{ $item->title }}</a>
+                                            <p class="text-warning">{{ trans('app.news_not_published') }}</p>
+                                        </div>
+                                        <div class="media-body" style="font-size: 12px;">
+                                            {{ trans('app.created_at') }}
+                                            <time datetime='{{ $item->created_at }}' title='{{ $item->created_at }}'>{{ \Carbon\Carbon::parse($item->created_at)->diffForHumans() }}</time>
+                                        </div>
                                     </div>
-                                </div>
-                            </li>
-                        @else
-                            @permission(('edit-news'))
-                            <li class="list-group-item active media" style="margin-top: 0px;">
-                                <a class="pull-right" href="{{ route('news.show', $item->id) }}"><span class="badge">{{ $item->comments->count() }}</span></a>
-                                <a class="pull-left" href="{{ url('users', $item->user->id) }}"><img class="media-object img-rounded" width="42px" src="//{{ config('app.avatar_path') }}?size=42&gender=male&id={{ $item->user->id }}" alt="{{ $item->user->name }}"></a>
-                                <div class="thread-info">
-                                    <div class="media-heading">
-                                        <a href="{{ route('news.show', $item->id) }}">{{ $item->title }}</a>
-                                        <p class="text-warning">{{ trans('app.news_not_published') }}</p>
-                                    </div>
-                                    <div class="media-body" style="font-size: 12px;">
-                                        {{ trans('app.created_at') }}
-                                        <time datetime='{{ $item->created_at }}' title='{{ $item->created_at }}'>{{ \Carbon\Carbon::parse($item->created_at)->diffForHumans() }}</time>
-                                    </div>
-                                </div>
-                            </li>
-                            @endpermission
-                        @endif
-                    @endforeach
-                </ul>
-                <div class="card-footer">
-                    {{ $news->links('vendor.pagination.bootstrap-4') }}
+                                </li>
+                                @endpermission
+                            @endif
+                        @endforeach
+                    </ul>
+                    <div class="card-footer">
+                        {{ $news->links('vendor.pagination.bootstrap-4') }}
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -34,113 +34,125 @@
             </div>
         </div>
         <div class="row">
-            <div class="card">
-                <div class="card-header">{{ $news->title }}</div>
-                <div class="card-body">{!! \App\Helpers\InlineBoxHelper::GameBox($news->news_html) !!}</div>
-                <div class="card-footer">
-                    {{ trans('app.submitted_by') }}
-                    <a href='{{ url('users', $news->user_id) }}'>{{ $news->name }}</a> ::
-                    <time datetime='{{ $news->created_at }}' title='{{ $news->created_at }}'>{{ \Carbon\Carbon::parse($news->created_at)->diffForHumans() }}</time>
+            <div class="col-md-12 mb-3">
+                <div class="card">
+                    <div class="card-header">{{ $news->title }}</div>
+                    <div class="card-body">{!! \App\Helpers\InlineBoxHelper::GameBox($news->news_html) !!}</div>
+                    <div class="card-footer">
+                        {{ trans('app.submitted_by') }}
+                        <a href='{{ url('users', $news->user_id) }}'>{{ $news->name }}</a> ::
+                        <time datetime='{{ $news->created_at }}' title='{{ $news->created_at }}'>{{ \Carbon\Carbon::parse($news->created_at)->diffForHumans() }}</time>
+                    </div>
                 </div>
             </div>
         </div>
         <div class="row">
-            <div class="card">
-                <div class="card-header">{{ trans('app.popularity_helper') }}</div>
-                <div class="card-body">
-                    <p>{{ trans('app.use_the_popularity_helper') }}</p>
-                    <input type='text' value='{{ Request::fullUrl() }}' size='50' readonly='readonly'/>
+            <div class="col-md-12 mb-3">
+                <div class="card">
+                    <div class="card-header">{{ trans('app.popularity_helper') }}</div>
+                    <div class="card-body">
+                        <p>{{ trans('app.use_the_popularity_helper') }}</p>
+                        <input type='text' value='{{ Request::fullUrl() }}' size='50' readonly='readonly'/>
+                    </div>
                 </div>
             </div>
         </div>
         @if($news->comments()->count() > 0)
             <div class="row">
-                <div class="card">
-                    <div class="card-header">{{ trans('app.comments') }}</div>
-                    <div class="card-body">
-                        @foreach($news->comments()->get() as $comment)
-                            <div class="media">
-                                <div class="media-left">
-                                    <a href='{{ url('users', $comment->user_id) }}'
-                                       title="{{ $comment->user->name }}">
-                                        <img
-                                                width="32px"
-                                                src='//{{ config('app.avatar_path') }}?gender=male&id={{ $comment->user_id }}'
-                                                alt="{{ $comment->user->name }}" class='media img-rounded'/>
-                                    </a>
-                                </div>
-                                <div class="media-body">
-                                    <div class="media-heading">
-                                        <a href='{{ url('users', $comment->user_id) }}' title="{{ $comment->user->name }}">{{ $comment->user->name }}</a> -
-                                        {{ trans('app.posted_at') }} {{ $comment->created_at }}
-                                        @if($comment->vote_up == 1 and $comment->vote_down == 0)
-                                            <span class='vote up'>up</span>
-                                        @elseif($comment->vote_up == 0 and $comment->vote_down == 1)
-                                            <span class='vote down'>down</span>
-                                        @endif
+                <div class="col-md-12 mb-3">
+                    <div class="card">
+                        <div class="card-header">{{ trans('app.comments') }}</div>
+                        <div class="card-body">
+                            @foreach($news->comments()->get() as $comment)
+                                <div class="media">
+                                    <div class="media-left">
+                                        <a href='{{ url('users', $comment->user_id) }}'
+                                           title="{{ $comment->user->name }}">
+                                            <img
+                                                    style="width: 48px;" class="mr-3"
+                                                    src='//{{ config('app.avatar_path') }}?gender=male&id={{ $comment->user_id }}'
+                                                    alt="{{ $comment->user->name }}"/>
+                                        </a>
                                     </div>
-                                    <a href='{{ url('user', $comment->user_id) }}'
-                                       class='user'>{{ $comment->name }}</a>
-                                    {!! \App\Helpers\InlineBoxHelper::GameBox($comment->comment_html) !!}
+                                    <div class="media-body">
+                                        <div class="media-heading">
+                                            <a href='{{ url('users', $comment->user_id) }}' title="{{ $comment->user->name }}">{{ $comment->user->name }}</a> -
+                                            {{ trans('app.posted_at') }} {{ $comment->created_at }}
+                                            @if($comment->vote_up == 1 and $comment->vote_down == 0)
+                                                <span class='vote up'>up</span>
+                                            @elseif($comment->vote_up == 0 and $comment->vote_down == 1)
+                                                <span class='vote down'>down</span>
+                                            @endif
+                                        </div>
+                                        <a href='{{ url('user', $comment->user_id) }}'
+                                           class='user'>{{ $comment->name }}</a>
+                                        {!! \App\Helpers\InlineBoxHelper::GameBox($comment->comment_html) !!}
+                                    </div>
                                 </div>
-                            </div>
-                        @endforeach
+                            @endforeach
+                        </div>
                     </div>
                 </div>
             </div>
         @else
             <div class="row">
-                <div class="card">
-                    <div class="card-header">{{ trans('app.comments') }}</div>
-                    <div class="card-body">
-                        {{ trans('app.no_comments_available') }}
+                <div class="col-md-12 mb-3">
+                    <div class="card">
+                        <div class="card-header">{{ trans('app.comments') }}</div>
+                        <div class="card-body">
+                            {{ trans('app.no_comments_available') }}
+                        </div>
                     </div>
                 </div>
             </div>
         @endif
         <div class="row">
-            <div class="card">
-                <div class="card-header">{{ trans('app.comment_rules') }}</div>
-                <div class="card-body">
-                    <p>{{ trans('app.comment_rule_1') }}</p>
-                    <p>{{ trans('app.comment_rule_2') }}</p>
-                    <p>{{ trans('app.comment_rule_3') }}</p>
-                    <p>{{ trans('app.comment_rule_4') }}</p>
+            <div class="col-md-12 mb-3">
+                <div class="card">
+                    <div class="card-header">{{ trans('app.comment_rules') }}</div>
+                    <div class="card-body">
+                        <p>{{ trans('app.comment_rule_1') }}</p>
+                        <p>{{ trans('app.comment_rule_2') }}</p>
+                        <p>{{ trans('app.comment_rule_3') }}</p>
+                        <p>{{ trans('app.comment_rule_4') }}</p>
+                    </div>
                 </div>
             </div>
         </div>
         <div class="row">
-            <div class="card">
-                <div class="card-header">{{ trans('app.add_comment') }}</div>
-                <div class="card-body">
-                    @permission(('create-game-comments'))
-                    {!! Form::open(['action' => ['CommentController@add']]) !!}
-                    {!! Form::hidden('content_id', $news->id) !!}
-                    {!! Form::hidden('content_type', 'news') !!}
-                    <div class='content'>
-                        @if(\App\Helpers\CheckRateableHelper::checkRateable('news', $news->gameid, Auth::id()) === true)
-                            <div id='prodvote'>
-                                {{ trans('app.rate_this_news') }}<br>
-                                <input type='radio' name='rating' id='ratingrulez' value='up'/>
-                                <label for='ratingrulez'>{{ trans('app.rate_up') }}</label>
-                                <input type='radio' name='rating' id='ratingpig' value='neut' checked='checked'/>
-                                <label for='ratingpig'>{{ trans('app.rate_neut') }}</label>
-                                <input type='radio' name='rating' id='ratingsucks' value='down'/>
-                                <label for='ratingsucks'>{{ trans('app.rate_down') }}</label>
-                            </div>
-                        @endif
+            <div class="col-md-12 mb-3">
+                <div class="card">
+                    <div class="card-header">{{ trans('app.add_comment') }}</div>
+                    <div class="card-body">
+                        @permission(('create-game-comments'))
+                        {!! Form::open(['action' => ['CommentController@add']]) !!}
+                        {!! Form::hidden('content_id', $news->id) !!}
+                        {!! Form::hidden('content_type', 'news') !!}
+                        <div class='content'>
+                            @if(\App\Helpers\CheckRateableHelper::checkRateable('news', $news->gameid, Auth::id()) === true)
+                                <div id='prodvote'>
+                                    {{ trans('app.rate_this_news') }}<br>
+                                    <input type='radio' name='rating' id='ratingrulez' value='up'/>
+                                    <label for='ratingrulez'>{{ trans('app.rate_up') }}</label>
+                                    <input type='radio' name='rating' id='ratingpig' value='neut' checked='checked'/>
+                                    <label for='ratingpig'>{{ trans('app.rate_neut') }}</label>
+                                    <input type='radio' name='rating' id='ratingsucks' value='down'/>
+                                    <label for='ratingsucks'>{{ trans('app.rate_down') }}</label>
+                                </div>
+                            @endif
 
-                        @include('_partials.markdown_editor')
+                            @include('_partials.markdown_editor')
 
-                        <div><a href='/?page=faq#markdown'>{{ trans('app.markdown_is_usable_here') }}</a></div>
+                            <div><a href='/?page=faq#markdown'>{{ trans('app.markdown_is_usable_here') }}</a></div>
+                        </div>
+                        <div class='foot'>
+                            <input type='submit' value='Submit' id='submit'>
+                        </div>
+                        {!! Form::close() !!}
+                        @else
+                            {{ trans('app.your_permissions_are_to_low') }}
+                            @endpermission
                     </div>
-                    <div class='foot'>
-                        <input type='submit' value='Submit' id='submit'>
-                    </div>
-                    {!! Form::close() !!}
-                    @else
-                        {{ trans('app.your_permissions_are_to_low') }}
-                        @endpermission
                 </div>
             </div>
         </div>
