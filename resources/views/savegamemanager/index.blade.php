da28b636123f7c99774506437660da88c65ff3b6
Marcel Hering <ryg@banncity.de>
2018-02-27 18:15:26 +0100

    savegamemanager index finished

--- resources/views/savegamemanager/index.blade.php
+++ resources/views/savegamemanager/index.blade.php
@@ -3,46 +3,50 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="page-header">
-                <h1>{{ trans('app.savegame_manager') }}</h1>
-                {!! Breadcrumbs::render('savegamemanager.index') !!}
+            <div class="col-md-12">
+                <div class="page-header">
+                    <h1>{{ trans('app.savegame_manager') }}</h1>
+                    {!! Breadcrumbs::render('savegamemanager.index') !!}
+                </div>
             </div>
         </div>
         <div class="row">
-            <div class="card">
-                <div class="card-header">
-                    {{ trans('app.list_with_savegames_newest_on_top') }}
-                </div>
-                <ul class="list-group">
-                    @foreach($games as $g)
-                        <li class="list-group-item">
-                            <div class="pull-right">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        {{ trans('app.list_with_savegames_newest_on_top') }}
+                    </div>
+                    <ul class="list-group">
+                        @foreach($games as $g)
+                            <li class="list-group-item">
+                                <div class="pull-right">
                                 <span class="badge">
                                     {{ \App\Models\GamesSavegame::whereUserId(Auth::id())->where('gamefile_id', '=', $g->gamefile_id)->get()->count() }} {{ trans('app.savegames') }}
                                 </span>
-                            </div>
-                            <a href="{{ action('SavegameManagerController@show', [$g->gamefile->id]) }}">
+                                </div>
+                                <a href="{{ action('SavegameManagerController@show', [$g->gamefile->id]) }}">
                                 <span class="typeiconlist">
                                     <span class="typei type_{{ $g->gamefile->gamefiletype->short }}" title="{{$g->gamefile->gamefiletype->title}}">{{ $g->gamefile->gamefiletype->title }}</span>
                                 </span>
-                                <span class="platformiconlist">
+                                    <span class="platformiconlist">
                                     <span class="typei type_{{ $g->gamefile->game->maker->short }}" title="{{ $g->gamefile->game->maker->title }}">
                                         {{ $g->gamefile->game->maker->title }}
                                     </span>
                                 </span>
-                                {{ $g->gamefile->game->title }}
-                                @if($g->gamefile->game->subtitle)
+                                    {{ $g->gamefile->game->title }}
+                                    @if($g->gamefile->game->subtitle)
+                                        -
+                                        <small>{{ $g->gamefile->game->subtitle }}</small>
+                                    @endif
                                     -
-                                    <small>{{ $g->gamefile->game->subtitle }}</small>
-                                @endif
-                                -
-                                {{ $g->gamefile->release_version }}
-                            </a>
+                                    {{ $g->gamefile->release_version }}
+                                </a>
 
-                            <span class="text-muted"> :: {{ trans('app.last_savegame') }} {{ \Carbon\Carbon::parse($g->updated_at)->diffForHumans() }}</span>
-                        </li>
-                    @endforeach
-                </ul>
+                                <span class="text-muted"> :: {{ trans('app.last_savegame') }} {{ \Carbon\Carbon::parse($g->updated_at)->diffForHumans() }}</span>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
