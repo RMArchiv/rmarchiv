85e692de887bf05268aa0f7c92fe6f4ac9703531
Marcel Hering <ryg@banncity.de>
2018-02-28 16:19:43 +0100

    add ban function

--- app/Http/Controllers/UserBanController.php
+++ app/Http/Controllers/UserBanController.php
@@ -18,7 +18,26 @@ class UserBanController extends Controller
     }
 
     public function ban(Request $request ,$userid){
+        $user = User::whereId($userid)->first();
 
+        $exp = 0;
+
+        if($request->exists('forever')){
+            $exp = '+ 150 years';
+        }elseif($request->exists('aday')){
+            $exp = '+1 day';
+        }elseif($request->exists('aweek')){
+            $exp = '+1 week';
+        }elseif($request->exists('amonth')){
+            $exp = '+1 month';
+        }
+
+        $user->ban([
+            'comment' => $request->get('banreason'),
+            'expired_at' => $exp,
+        ]);
+
+        return redirect(action('UserBanController@show', $userid));
     }
 
     public function unban(Request $request, $userid){
