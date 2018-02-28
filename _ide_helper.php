a5905d0b8b8c3fe1f39498615f48744cdcc4f740
Marcel Hering <ryg@banncity.de>
2018-02-28 14:23:29 +0100

    add cybercog/laravel-ban

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.4.36 on 2018-02-26 13:57:43.
+ * Generated for Laravel 5.4.36 on 2018-02-28 13:22:57.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
