e333ae378b714e36990e2efc1abc5777fbe4654a
Marcel Hering <ryg@banncity.de>
2018-03-01 10:58:32 +0100

    create game view fixed

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -3,9 +3,11 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="page-header">
-                <h1>{{ trans('app.games') }}</h1>
-                {!! Breadcrumbs::render('games') !!}
+            <div class="col-md-12">
+                <div class="page-header">
+                    <h1>{{ trans('app.games') }}</h1>
+                    {!! Breadcrumbs::render('games') !!}
+                </div>
             </div>
         </div>
         <div class="row">
