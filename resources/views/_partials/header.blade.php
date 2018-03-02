32caea91f6c7b4876dc18fb25e522b3e10d2b62e
Marcel Hering <ryg@banncity.de>
2018-03-02 14:41:04 +0100

    Update to Laravel 5.5

--- resources/views/_partials/header.blade.php
+++ resources/views/_partials/header.blade.php
@@ -1,5 +1,13 @@
 <div>
     @php
+        $logo = \DB::table('logos')
+        ->leftJoin('users', 'logos.user_id', '=', 'users.id')
+        ->leftJoin('logo_votes', 'logos.id', '=', 'logo_votes.logo_id')
+        ->select(['logos.title', 'logos.filename', 'users.name', 'users.id'])
+        ->whereRaw('(logo_votes.up - logo_votes.down) > 0')
+        ->inRandomOrder()
+        ->first();
+
         $fname = str_replace('storage/logos/', '',$logo->filename)
     @endphp
     <a href="/">
