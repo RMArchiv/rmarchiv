b095bcc0f7a0d6f7c9558b1d1103830ff1c89f10
Marcel Hering <ryg@banncity.de>
2018-03-03 00:06:46 +0100

    fix useronline list

--- resources/views/users/online.blade.php
+++ resources/views/users/online.blade.php
@@ -3,34 +3,38 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="page-header">
-                <h1>{{ trans('app.users_online') }}</h1>
-                {!! Breadcrumbs::render('online') !!}
+            <div class="col-md-12">
+                <div class="page-header">
+                    <h1>{{ trans('app.users_online') }}</h1>
+                    {!! Breadcrumbs::render('online') !!}
+                </div>
             </div>
         </div>
         <div class="row">
-            <table class="table table-striped">
-                <thead>
-                <tr>
-                    <th>
-                        {{ trans('app.username') }}
-                    </th>
-                    <th>
-                        {{ trans('app.last_page_visit') }}
-                    </th>
-                    <th>
-                        {{ trans('app.last_action_date') }}
-                    </th>
-                </tr>
-                </thead>
-                @foreach($uo as $u)
+            <div class="col-md-12">
+                <table class="table table-striped">
+                    <thead>
                     <tr>
-                        <td><a href="{{ action('UserController@show', $u->user_id) }}">{{ $u->user->name }}</a></td>
-                        <td><a href="{{ $u->last_place }}">{{ $u->last_place }}</a></td>
-                        <td>{{ $u->created_at }}</td>
+                        <th>
+                            {{ trans('app.username') }}
+                        </th>
+                        <th>
+                            {{ trans('app.last_page_visit') }}
+                        </th>
+                        <th>
+                            {{ trans('app.last_action_date') }}
+                        </th>
                     </tr>
-                @endforeach
-            </table>
+                    </thead>
+                    @foreach($uo as $u)
+                        <tr>
+                            <td><a href="{{ action('UserController@show', $u->user_id) }}">{{ $u->user->name }}</a></td>
+                            <td><a href="{{ $u->last_place }}">{{ $u->last_place }}</a></td>
+                            <td>{{ $u->created_at }}</td>
+                        </tr>
+                    @endforeach
+                </table>
+            </div>
         </div>
     </div>
 @endsection
