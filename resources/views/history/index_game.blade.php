86cdb3a8c8122aebc780bc341fd608d4017a5836
Marcel Hering <ryg@banncity.de>
2018-03-02 10:06:26 +0100

    Gamefiles Edit: Breadcrumb finished

--- resources/views/history/index_game.blade.php
+++ resources/views/history/index_game.blade.php
@@ -3,49 +3,53 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="page-header">
-                <h1>{{ trans('app.changelog') }}</h1>
-                {!! Breadcrumbs::render('game.changelog', $game) !!}
+            <div class="col-md-12">
+                <div class="page-header">
+                    <h1>{{ trans('app.changelog') }}</h1>
+                    {!! Breadcrumbs::render('game.changelog', $game) !!}
+                </div>
             </div>
         </div>
         <div class="row">
-            <div class="card">
-                <div class="card-header">
-                    {{ trans('app.changelog') }}
-                </div>
-                <table class="table table-striped table-bordered table-list">
-                    <thead>
-                    <tr>
-                        <th>{{ trans('app.activity') }}</th>
-                        <th>{{ trans('app.date') }}</th>
-                        <th>{{ trans('app.activity_by') }}</th>
-                        <th>{{ trans('app.change') }}</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($activity as $a)
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        {{ trans('app.changelog') }}
+                    </div>
+                    <table class="table table-sm">
+                        <thead>
                         <tr>
-                            <td>
-                                {{ $a->description }}
-                            </td>
-                            <td>
-                                {{ $a->created_at }}
-                            </td>
-                            <td>
-                                <a href="{{ action('UserController@show', $a->causer->id) }}" class="usera" title="{{ $a->causer->name }}">
-                                    <img width="16px" src="//{{ config('app.avatar_path') }}?gender=male&amp;id={{ $a->causer->id }}" alt="{{ $a->causer->name }}" class="avatar">
-                                </a>
-                                <a href="{{ action('UserController@show', $a->causer->id) }}" class="user">{{ $a->causer->name }}</a>
-                            </td>
-                            <td>
-                                @if($a->description == 'updated')
-                                    {{ implode(', ', array_keys(\App\Helpers\MiscHelper::array_diff_assoc_recursive($a->changes['old'], $a->changes['attributes']))) }}
-                                @endif
-                            </td>
+                            <th>{{ trans('app.activity') }}</th>
+                            <th>{{ trans('app.date') }}</th>
+                            <th>{{ trans('app.activity_by') }}</th>
+                            <th>{{ trans('app.change') }}</th>
                         </tr>
-                    @endforeach
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                        @foreach($activity as $a)
+                            <tr>
+                                <td>
+                                    {{ $a->description }}
+                                </td>
+                                <td>
+                                    {{ $a->created_at }}
+                                </td>
+                                <td>
+                                    <a href="{{ action('UserController@show', $a->causer->id) }}" class="usera" title="{{ $a->causer->name }}">
+                                        <img width="16px" src="//{{ config('app.avatar_path') }}?gender=male&amp;id={{ $a->causer->id }}" alt="{{ $a->causer->name }}" class="avatar">
+                                    </a>
+                                    <a href="{{ action('UserController@show', $a->causer->id) }}" class="user">{{ $a->causer->name }}</a>
+                                </td>
+                                <td>
+                                    @if($a->description == 'updated')
+                                        {{ implode(', ', array_keys(\App\Helpers\MiscHelper::array_diff_assoc_recursive($a->changes['old'], $a->changes['attributes']))) }}
+                                    @endif
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
