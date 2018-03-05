4f5aca38154608d2b7380466b9aec321a2fc91bd
Marcel Hering <ryg@banncity.de>
2018-03-05 08:14:13 +0100

    Put Missing files into Public File

--- app/Console/Commands/GetMissingGameFilesCommand.php
+++ app/Console/Commands/GetMissingGameFilesCommand.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\GamesFile;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 
 class GetMissingGameFilesCommand extends Command
@@ -40,13 +41,22 @@ class GetMissingGameFilesCommand extends Command
     {
         $gf = GamesFile::all();
         $i = 0;
+        $content = '';
+
+        $doc_root = print_r(scandir('./public'));
+        echo $doc_root;
 
         foreach ($gf as $g){
             $filepath = storage_path('app/public/'.$g->filename);
             if (!file_exists($filepath)){
                 $i +=1;
-                echo $g->game->title.' - '.$g->game->subtitle.' - ID:'.$g->game_id.' - Version:'.$g->release_version.' - up by:'.$g->user->name.' - uuid: '.$g->filename.PHP_EOL;
+                $content = "Missing Gamefiles:".PHP_EOL;
+                $content .= 'Created at: '.Carbon::now()->toDateTimeString().PHP_EOL;
+                $content .= '----------------------------------------------------------------'.PHP_EOL;
+                $content .= $g->game->title.' - '.$g->game->subtitle.' - ID:'.$g->game_id.' - Version:'.$g->release_version.' - up by:'.$g->user->name.' - uuid: '.$g->filename.PHP_EOL;
             }
         }
+
+        file_put_contents($doc_root.'/missfiles.txt', $content);
     }
 }
