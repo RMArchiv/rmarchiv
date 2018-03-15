3adf45c4ee0bfdb22af0df2a6d85850ec364c7f7
Marcel Hering <ryg@banncity.de>
2018-03-15 22:26:51 +0100

    fix col width

--- resources/views/board/post/edit.blade.php
+++ resources/views/board/post/edit.blade.php
@@ -2,38 +2,42 @@
 @section('pagetitle', trans('app.edit_post'))
 @section('content')
     <div class="container">
-        <div class="row">
-            <div class="page-header">
-                <h1>{{ trans('app.edit_post') }}</h1>
-                {!! Breadcrumbs::render('post.edit', \App\Models\BoardCat::whereId($post->cat_id)->first(), $post) !!}
+        <div class="col-md-12">
+            <div class="row">
+                <div class="page-header">
+                    <h1>{{ trans('app.edit_post') }}</h1>
+                    {!! Breadcrumbs::render('post.edit', \App\Models\BoardCat::whereId($post->cat_id)->first(), $post) !!}
+                </div>
             </div>
         </div>
+
         @if(Auth::check())
             @if(Auth::user()->id == $post->user_id or Auth::user()->can('mod-threads'))
-                <div class="row">
-                    <div class="card">
-                        <div class="card-body">
-                            <form action="{{ route('board.post.update', [$post->thread_id, $post->id]) }}" method="post" enctype="multipart/form-data">
-                                {{ csrf_field() }}
+                <div class="col-md-12">
+                    <div class="row">
+                        <div class="card">
+                            <div class="card-body">
+                                <form action="{{ route('board.post.update', [$post->thread_id, $post->id]) }}" method="post" enctype="multipart/form-data">
+                                    {{ csrf_field() }}
 
                                     @if (count($errors) > 0))
-                                        <div class="rmarchivtbl errorbox">
-                                            <h2>{{ trans('app.edit_post_failed') }}</h2>
-                                            <div class="content">
-                                                @foreach ($errors->all() as $error)
-                                                    <strong>{{ $error }}</strong>
-                                                @endforeach
-                                            </div>
+                                    <div class="rmarchivtbl errorbox">
+                                        <h2>{{ trans('app.edit_post_failed') }}</h2>
+                                        <div class="content">
+                                            @foreach ($errors->all() as $error)
+                                                <strong>{{ $error }}</strong>
+                                            @endforeach
                                         </div>
+                                    </div>
                                     @endif
 
                                     @if($post->thread->user_id == Auth::id() or Auth::user()->can('mod-threads'))
-                                    <div class="form-group">
-                                        <label for="title" class="col-lg-2 col-form-label">{{trans('app.change_board_title')}} *</label>
-                                        <div class="col-lg-10">
-                                            <input type="text" class="form-control" id="title" name="title" value="{{ $post->thread->title }}">
+                                        <div class="form-group">
+                                            <label for="title" class="col-lg-2 col-form-label">{{trans('app.change_board_title')}} *</label>
+                                            <div class="col-lg-10">
+                                                <input type="text" class="form-control" id="title" name="title" value="{{ $post->thread->title }}">
+                                            </div>
                                         </div>
-                                    </div>
                                     @endif
 
                                     <input type="hidden" name="thread_id" id="thread_id" value="{{ $post->thread_id }}">
@@ -43,10 +47,12 @@
                                     <div class="foot">
                                         <input type="submit" value="{{ trans('app.submit') }}">
                                     </div>
-                            </form>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
+
             @else
                 <div class="row">
                     <div class="card">
