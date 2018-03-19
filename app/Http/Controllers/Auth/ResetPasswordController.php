357d9d3817a84af5bdc96b913daaac591a8dea27
Marcel Hering <ryg@banncity.de>
2018-03-19 09:52:44 +0100

    Password Reset preperation

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -30,7 +30,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
