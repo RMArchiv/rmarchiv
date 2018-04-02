244c95058896ce99a45d9c902578d9b29f473806
Marcel Hering <ryg@banncity.de>
2018-04-02 09:47:06 +0200

    debug logo rating.

--- resources/views/_partials/header.blade.php
+++ resources/views/_partials/header.blade.php
@@ -8,10 +8,9 @@
         ->inRandomOrder()
         ->first();
 
-        $fname = str_replace('storage/logos/', '',$logo->filename)
     @endphp
     <a href="/">
-        <img class="mx-auto d-block" height="100px" src="{{ url('logo/'.$fname) }}" alt="Logo: {{ $logo->title }}"/>
+        <img class="mx-auto d-block" height="100px" src="{{ asset($logo->filename) }}" alt="Logo: {{ $logo->title }}"/>
     </a>
     <p>logo '{{ $logo->title }}' by <a href='{{ url('users', $logo->id) }}' class='user'>{{ $logo->name }}</a> :: {{ config('app.name') }} is brought to you with love.</p>
 </div>
