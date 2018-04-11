b9d50611382ee602c501219c7079c9f36dbbe35b
Marcel Hering <ryg@banncity.de>
2018-04-11 16:16:19 +0200

    add model cache

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-04-11 14:03:40.
+    * Generated on 2018-04-11 14:16:05.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.5.39 on 2018-04-11 14:03:39.
+ * Generated for Laravel 5.5.39 on 2018-04-11 14:16:04.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
