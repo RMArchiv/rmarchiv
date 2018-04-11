2d56ecef4ee26abeb15225722dd0ab5cc33e37db
Marcel Hering <ryg@banncity.de>
2018-04-11 16:21:33 +0200

    redis caching for news model

--- app/Models/News.php
+++ app/Models/News.php
@@ -7,6 +7,7 @@
 
 namespace App\Models;
 
+use GeneaLabs\LaravelModelCaching\Traits\Cachable;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -39,6 +40,7 @@ use Illuminate\Database\Eloquent\Model;
  */
 class News extends Model
 {
+    use Cachable;
     use \Venturecraft\Revisionable\RevisionableTrait;
     public $timestamps = true;
     protected $table = 'news';
