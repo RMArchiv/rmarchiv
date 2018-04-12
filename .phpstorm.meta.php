4ea62a2a8eecc59fc6adc0dd636ec7b3f37622de
Marcel Hering <ryg@banncity.de>
2018-04-12 13:35:56 +0200

    fix blockquote

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-04-11 14:16:05.
+    * Generated on 2018-04-12 11:10:27.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.5.39 on 2018-04-11 14:16:04.
+ * Generated for Laravel 5.5.40 on 2018-04-12 11:10:26.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
