e4db781ba32137bfdf883e5b19b82a80e3402d29
rygos <rygos@icloud.com>
2020-06-15 08:14:46 +0200

    change URL to rmarchiv.de

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -101,20 +101,20 @@
 
 <!-- Matomo -->
 <script type="text/javascript">
-    var _paq = _paq || [];
-    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-    @if(Auth::check())
-        _paq.push(['setUserId', '{{ Auth::user()->id }}'])
-    @endif
-    _paq.push(['trackPageView']);
-    _paq.push(['enableLinkTracking']);
-    (function() {
-        var u="//stats.rmarchiv.de/";
-        _paq.push(['setTrackerUrl', u+'piwik.php']);
-        _paq.push(['setSiteId', '1']);
-        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-    })();
+	var _paq = window._paq || [];
+	/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+	@if(Auth::check())
+	_paq.push(['setUserId', '{{ Auth::user()->id }}'])
+	@endif
+	_paq.push(['trackPageView']);
+	_paq.push(['enableLinkTracking']);
+	(function() {
+		var u="//stats.rmarchiv.de/";
+		_paq.push(['setTrackerUrl', u+'matomo.php']);
+		_paq.push(['setSiteId', '1']);
+		var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+		g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+	})();
 </script>
 <!-- End Matomo Code -->
 </html>
