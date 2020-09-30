6abfc250e6c5596afa7b92ff73d0b1abba4a6538
rygos <rygos@icloud.com>
2020-09-30 22:04:43 +0200

    first fix for ressource index

--- resources/views/resources/_partials/nav.blade.php
+++ resources/views/resources/_partials/nav.blade.php
@@ -1,8 +1,6 @@
-<nav id="asd">
-    <ul>
-        <li><a href="{{ url('/resources/gfx') }}">{{ trans('app.gfx') }}</a></li>
-        <li><a href="{{ url('/resources/sfx') }}">{{ trans('app.sfx') }}</a></li>
-        <li><a href="{{ url('/resources/scripts') }}">{{ trans('app.scripts') }}</a></li>
-        <li><a href="{{ url('/resources/tools') }}">{{ trans('app.tools') }}</a></li>
-    </ul>
-</nav>
+<ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="{{ url('/resources/gfx') }}">{{ trans('app.gfx') }}</a></li>
+    <li class="breadcrumb-item"><a href="{{ url('/resources/sfx') }}">{{ trans('app.sfx') }}</a></li>
+    <li class="breadcrumb-item"><a href="{{ url('/resources/scripts') }}">{{ trans('app.scripts') }}</a></li>
+    <li class="breadcrumb-item active" aria-current="page"><a href="{{ url('/resources/tools') }}">{{ trans('app.tools') }}</a></li>
+</ol>
--- resources/views/resources/index.blade.php
+++ resources/views/resources/index.blade.php
@@ -13,37 +13,39 @@
             </div>
         </div>
         <div class="row">
-            <div class="card">
-                <div class="card-header">{{ trans('app.statistics') }}</div>
-                <table class="table table-striped table-hover">
-                    @foreach($resources as $res)
-                        <tr>
-                            <td>{{ $res->restype }}</td>
-                            <td>{{ $res->rescat }}</td>
-                            <td class=>{{ $res->username }}</td>
-                            <td class='date'><time datetime='{{ $res->rescreatedat }}' title='{{ $res->rescreatedat }}'>{{ \Carbon\Carbon::parse($res->rescreatedat)->diffForHumans() }}</time></td>
-                            <td><a href="{{ route('resources.show', [$res->restype, $res->rescat, $res->resid]) }}">{{ $res->restitle }}</a></td>
-                            <td>{{ $res->contenttype }}</td>
-                            <td class='votes'>{{ $res->voteup or 0 }}</td>
-                            <td class='votes'>{{ $res->votedown or 0 }}</td>
-                            @php $avg = @(($res->voteup - $res->votedown) / ($res->voteup + $res->votedown)) @endphp
-                            <td class='votes'>{{ number_format($avg, 2) }}&nbsp;
-                                @if($avg > 0)
-                                    <img src='/assets/rate_up.gif' alt='{{ trans('app.rate_up') }}'/>
-                                @elseif($avg == 0)
-                                    <img src='/assets/rate_neut.gif' alt='{{ trans('app.rate_neut') }}'/>
-                                @elseif($avg < 0)
-                                    <img src='/assets/rate_down.gif' alt='{{ trans('app.rate_down') }}'/>
-                                @endif
-                            </td>
-                            @php
-                                $perc = \App\Helpers\MiscHelper::getPopularity($res->commentcount, $commentsmax);
-                            @endphp
-                            <td><div class='innerbar_solo' style='width: {{ $perc }}%' title='{{ number_format($perc, 2) }}%'><span>{{ $perc }}</span></div></td>
-                            <td>{{ $res->commentcount }}</td>
-                        </tr>
-                    @endforeach
-                </table>
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">{{ trans('app.resources_overview') }}</div>
+                    <table class="table table-striped table-hover">
+                        @foreach($resources as $res)
+                            <tr>
+                                <td>{{ $res->restype }}</td>
+                                <td>{{ $res->rescat }}</td>
+                                <td class=>{{ $res->username }}</td>
+                                <td class='date'><time datetime='{{ $res->rescreatedat }}' title='{{ $res->rescreatedat }}'>{{ \Carbon\Carbon::parse($res->rescreatedat)->diffForHumans() }}</time></td>
+                                <td><a href="{{ route('resources.show', [$res->restype, $res->rescat, $res->resid]) }}">{{ $res->restitle }}</a></td>
+                                <td>{{ $res->contenttype }}</td>
+                                <td class='votes'>{{ $res->voteup or 0 }}</td>
+                                <td class='votes'>{{ $res->votedown or 0 }}</td>
+                                @php $avg = @(($res->voteup - $res->votedown) / ($res->voteup + $res->votedown)) @endphp
+                                <td class='votes'>{{ number_format($avg, 2) }}&nbsp;
+                                    @if($avg > 0)
+                                        <img src='/assets/rate_up.gif' alt='{{ trans('app.rate_up') }}'/>
+                                    @elseif($avg == 0)
+                                        <img src='/assets/rate_neut.gif' alt='{{ trans('app.rate_neut') }}'/>
+                                    @elseif($avg < 0)
+                                        <img src='/assets/rate_down.gif' alt='{{ trans('app.rate_down') }}'/>
+                                    @endif
+                                </td>
+                                @php
+                                    $perc = \App\Helpers\MiscHelper::getPopularity($res->commentcount, $commentsmax);
+                                @endphp
+                                <td><div class='innerbar_solo' style='width: {{ $perc }}%' title='{{ number_format($perc, 2) }}%'><span>{{ $perc }}</span></div></td>
+                                <td>{{ $res->commentcount }}</td>
+                            </tr>
+                        @endforeach
+                    </table>
+                </div>
             </div>
         </div>
     </div>
