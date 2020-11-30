f4427e8cde7922ef55f6005b13bfc6a552622391
rygos <rygos@icloud.com>
2020-11-30 12:56:52 +0100

    Report Test

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -27,7 +27,7 @@
                                 </a> <a href="{{ url('/user', $r->user_id) }}" class="user">{{ $r->user->name }}</a>
                             </td>
                             <td><time datetime='{{ $r->created_at }}' title='{{ $r->created_at }}'>{{ \Carbon\Carbon::parse($r->created_at)->diffForHumans() }}</time></td>
-                            <td>{{ $r->game->title }}</td>
+                            <td>{{ dd($r->game) }}</td>
                             <td>{{ $r->reason }}</td>
                             <td>{{ $r->closed }}</td>
                             <td>{{ $r->remarks }}</td>
