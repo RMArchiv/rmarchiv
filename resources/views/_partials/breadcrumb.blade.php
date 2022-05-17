be1691b084d92d0fe5e52f0b57fcaeabaa0cb7ce
ryg <rygos@icloud.com>
2022-05-17 22:43:51 +0200

    breadcrumb template update

--- resources/views/_partials/breadcrumb.blade.php
+++ resources/views/_partials/breadcrumb.blade.php
@@ -1,8 +1,8 @@
-@if ($breadcrumbs)
+@unless ($breadcrumbs->isEmpty())
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
         @foreach ($breadcrumbs as $breadcrumb)
-            @if ($breadcrumb->url && !$breadcrumb->last)
+            @if (!is_null($breadcrumb->url) && !$loop->last)
                 <li class="breadcrumb-item"><a href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a></li>
             @else
                 <li class="breadcrumb-item active" aria-current="page">{{ $breadcrumb->title }}</li>
@@ -10,4 +10,4 @@
         @endforeach
         </ol>
     </nav>
-@endif
+@endunless
