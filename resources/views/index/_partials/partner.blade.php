00e122dcea782fb4412aec6c4f9e6ed86f30718c
ryg <rygos@icloud.com>
2022-06-22 17:58:36 +0200

    Makerpendium parter picture

--- resources/views/index/_partials/partner.blade.php
+++ resources/views/index/_partials/partner.blade.php
@@ -5,8 +5,8 @@
                 {{ trans('Partner Seiten') }}
             </div>
             <div class="card-body">
-                <a href="./assets/makerpendium_neu.png" target="_blank">
-                    <img class="img-fluid center-block" src="https://www.makerpendium.de/images/makerpendium_neu.png" alt="Makerpendium Logo">
+                <a href="https://www.makerpendium.de" target="_blank">
+                    <img class="img-fluid center-block" src="/assets/makerpendium_neu.png" alt="Makerpendium Logo">
                 </a>
             </div>
         </div>
