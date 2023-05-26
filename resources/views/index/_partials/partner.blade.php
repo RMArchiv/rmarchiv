fc64b3f1e1044651edad28c5882186401ac667dc
Marcel Hering <rygos@icloud.com>
2023-05-26 08:13:53 +0200

    Center partnerlinks

--- resources/views/index/_partials/partner.blade.php
+++ resources/views/index/_partials/partner.blade.php
@@ -5,12 +5,12 @@
                 {{ trans('Partner Seiten') }}
             </div>
             <div class="card-body">
-                <p>
+                <p style="text-align: center">
                     <a href="https://www.makerpendium.de/" target="_blank">
                         <img class="img-fluid center-block" src="/assets/makerpendium_neu.png" alt="Makerpendium Logo">
                     </a>
                 </p>
-                <p>
+                <p style="text-align: center">
                     <a href="https://gamedevcafe.de/" target="_blank">
                         <img class="img-fluid center-block" src="/assets/gamedevbanner.png" alt="GameDevCafe Logo">
                     </a>
