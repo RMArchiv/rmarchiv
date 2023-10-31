abc1c2d86cd7ba651e2d43ccda382f835a481abd
Marcel Hering <rygos@icloud.com>
2023-11-01 00:22:16 +0100

    change datetime to timestamp

--- app/Http/Controllers/Api/Client/GamesController.php
+++ app/Http/Controllers/Api/Client/GamesController.php
@@ -11,7 +11,7 @@ use Illuminate\Http\Request;
 class GamesController extends Controller
 {
     public function index($datetime){
-        $games = Game::where('updated_at', '>=', Carbon::parse($datetime))->get();
+        $games = Game::where('updated_at', '>=', Carbon::createFromTimestamp($datetime))->get();
 
         $ret = [
             'info' => [
