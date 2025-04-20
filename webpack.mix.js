cd34dbc7ea47dbf331584d207ad7fe86682f30bd
wes <207344724+jwesn@users.noreply.github.com>
2025-04-20 12:18:42 +0200

    use jquery-ui css smothness node modules & fix asset compilation in external css

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,7 @@
 const mix = require('laravel-mix');
 
-mix.setPublicPath('public');
-mix.setResourceRoot('resources');
-
-mix.js('resources/assets/js/app.js', 'public/js')
-    .sass('resources/assets/sass/app.scss', 'public/css', {sassOptions: {
+mix.js('resources/assets/js/app.js', 'js')
+    .sass('resources/assets/sass/app.scss', 'css', {sassOptions: {
         style: "compressed"
     }
 });
