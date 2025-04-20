9a71f58061412a44b2b69878b53b15aaac6c2884
wes <207344724+jwesn@users.noreply.github.com>
2025-04-20 07:57:56 +0200

    Move editor scss out off codebase & patch original editor.md syntax error which prevents compilation & yarn added patch package

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,10 @@
 const mix = require('laravel-mix');
 
 mix.setPublicPath('public');
-mix.setResourceRoot('../');
+mix.setResourceRoot('resources');
 
 mix.js('resources/assets/js/app.js', 'public/js')
-    .sass('resources/assets/sass/app.scss', 'public/css', [
-    ]);
+    .sass('resources/assets/sass/app.scss', 'public/css', {sassOptions: {
+        style: "compressed"
+    }
+});
