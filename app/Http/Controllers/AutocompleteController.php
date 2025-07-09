763dd46384358196bb1a8f4b982b87ffa8e9c2dc
wes <207344724+jwesn@users.noreply.github.com>
2025-07-09 22:33:21 +0200

    improve game search - fixed developer query & developer games are shown on top now

--- app/Http/Controllers/AutocompleteController.php
+++ app/Http/Controllers/AutocompleteController.php
@@ -11,6 +11,7 @@ use App\Models\Developer;
 use App\Models\Game;
 use App\Models\Tag;
 use App\Models\User;
+use Illuminate\Support\Facades\Log;
 
 class AutocompleteController extends Controller
 {
@@ -47,13 +48,56 @@ class AutocompleteController extends Controller
         $games = Game::search($term)->get();
         // Simple dev game search - check performance
         $developers = Developer::search($term)->get();
-        foreach ($developers as $developer) {
+
+        foreach ($developers->reverse()->values() as $developer) {
             $devGames = Game::with('developers')
+            ->select(
+                    'games.id as id',
+                    'games.title as title',
+                    'games.subtitle as subtitle',
+                    'games.desc_md as desc_md',
+                    'games.desc_html as desc_html',
+                    'games.website_url as website_url',
+                    'games.user_id as user_id',
+                    'games.views as views',
+                    'games.release_date as release_date',
+                    'games.maker_id as maker_id',
+                    'games.deleted_at as deleted_at',
+                    'games.created_at as created_at',
+                    'games.updated_at as updated_at',
+                    'games.lang_id as lang_id',
+                    'games.atelier_id as atelier_id',
+                    'games.release_type as release_type',
+                    'games.voteup as voteup',
+                    'games.votedown as votedown',
+                    'games.avg as avg',
+                    'games.comments as comments',
+                    'games.desc_md_translation as desc_md_translation',
+                    'games.is_banned as is_banned',
+                    'games.is_banned_reason as is_banned_reason',
+                    'games.is_banned_at as is_banned_at',
+                    'games.invisible_on_start_page as invisible_on_start_page',
+                    'games.client_visible as client_visible',
+                    'games.nsfw as nsfw',)
                 ->leftJoin('games_developer as gd', 'gd.game_id', '=', 'games.id')
                 ->join('developer', 'gd.developer_id', '=', 'developer.id')
                 ->where('gd.developer_id', '=', $developer->id)->get();
-            $games = $games->merge($devGames);
+            $games = $devGames->merge($games);
         }
+        // TODO: Alternative - Compare performance with  - Update similar performance
+        // foreach ($developers->reverse()->values() as $developer) {
+        //     $idArray = Game::with('developers')
+        //         ->select(
+        //             'games.id as id')
+        //         ->leftJoin('games_developer as gd', 'gd.game_id', '=', 'games.id')
+        //         ->join('developer', 'gd.developer_id', '=', 'developer.id')
+        //         ->where('gd.developer_id', '=', $developer->id)->get()->toArray();
+        //     $devGamesId = array_map(
+        //         function($gameData) {return $gameData['id'];},
+        //         $idArray);
+        //     $devGames = Game::whereIn('id', $devGamesId)->get();
+        //     $games = $devGames->merge($games);
+        // }
 
         foreach ($games as $g) {
             $result[] = [
@@ -79,7 +123,7 @@ class AutocompleteController extends Controller
                 'developers' => \App\Helpers\DatabaseHelper::getDevelopersList($g->id),
 
                 'release' => \Carbon\Carbon::parse(\App\Helpers\DatabaseHelper::getReleaseDateFromGameId($g->id))->toDateString(),
-                'created' => \Carbon\Carbon::parse($g->created_at)->diffForHumans(),
+                // 'created' => \Carbon\Carbon::parse($g->created_at)->diffForHumans(),
 
                 'votesUp' => $g->voteup ?? 0,
                 'votesDown' => $g->votedown ?? 0,
@@ -87,7 +131,7 @@ class AutocompleteController extends Controller
                 'tags' => $g->tags,
                 'hasCdc' => $g->cdcs->count() > 0,
                 'screenshot' => route('screenshot.show', [$g->id, 1]),
-                'value' => \View::make('_partials.inline_gamebox', ['game' => $g])->render(),
+                'value' => "",
                 'average' => number_format(floatval($g->avg), 2),
                 'translation' => array(
                     'titleScreenAlt' => trans('app.titlescreen'),
