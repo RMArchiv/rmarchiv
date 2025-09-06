1fb510560cf1db07311ee1636d857c20c5bf0715
wes <207344724+jwesn@users.noreply.github.com>
2025-09-06 19:32:06 +0200

    extract games filter to separate component & fix password reset stylings

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -11,56 +11,7 @@
             </div>
         </div>
         {{-- filter --}}
-        <div class="d-flex gap-2 py-2">
-            <form name="maker" action="/games">
-                <div class="d-flex gap-2">
-                    <div class="input-group input-group-sm">
-                        <label class="input-group-text" for="maker-select">{{ trans('app.maker') }}</label>
-
-                        <select id="maker-select" class="form-select" name="maker" title="{{ trans('app.maker') }}">
-                            <option value="" selected>{{ trans('app.filter.all') }}</option>
-                            @foreach ($makers as $maker)
-                                @if ($maker->id == request()->get('maker'))
-                                    <option selected value="{{ $maker->id }}">{{ $maker->title }}</option>
-                                @else
-                                    <option value="{{ $maker->id }}">{{ $maker->title }}</option>
-                                @endif
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="input-group input-group-sm">
-                        <label class="input-group-text" for="language-select">{{ trans('app.language') }}</label>
-
-                        <select id="language-select" class="form-select" name="language" title="{{ trans('app.language') }}">
-                            <option value="" selected>{{ trans('app.filter.all') }}</option>
-                            @foreach ($languages as $language)
-                                @if ($language->id == request()->get('language'))
-                                    <option selected value="{{ $language->id }}">{{ $language->name }}</option>
-                                @else
-                                    <option value="{{ $language->id }}">{{ $language->name }}</option>
-                                @endif
-                            @endforeach
-                        </select>
-                    </div>
-
-                    <div class="input-group input-group-sm">
-                        <label class="input-group-text" for="tag-select">{{ trans('app.tags') }}</label>
-
-                        <select id="tag-select" class="form-select" name="tag" title="{{ trans('app.tags') }}">
-                            <option value="" selected>{{ trans('app.filter.all') }}</option>
-                            @foreach ($tags as $tag)
-                                @if ($tag->id == request()->get('tag'))
-                                    <option selected value="{{ $tag->id }}">{{ $tag->title }}</option>
-                                @else
-                                    <option value="{{ $tag->id }}">{{ $tag->title }}</option>
-                                @endif
-                            @endforeach
-                        </select>
-                    </div>
-                    <button class="btn btn-secondary" title="filter">Filter</button>
-                </div>
-            </form>
-        </div>
+        <x-games.filter :makers="$makers" :languages="$languages" :tags="$tags" />
         <div class="row">
             @include('_partials.tables.game_table', [
                 'games' => $games,
--- resources/views/maker/index.blade.php
+++ resources/views/maker/index.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="page-header">
-                    <h1>maker</h1>
+                    <h1>{{trans('app.maker')}}</h1>
                     {!! Breadcrumbs::render('maker') !!}
                 </div>
             </div>
