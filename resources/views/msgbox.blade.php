6c0f9d66fc7eaed83a35736aa33cd637d53b1116
wes <207344724+jwesn@users.noreply.github.com>
2025-09-21 12:55:14 +0200

    fix message styling

--- resources/views/msgbox.blade.php
+++ resources/views/msgbox.blade.php
@@ -1,13 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="rmarchivtbl successbox">
-        <h2>{!! $title !!}</h2>
-        <div class="content">
-            <strong>{!! $msg !!}</strong>
-        </div>
-        <div class="foot">
-            <a href="{!! $redirect_to !!}">{!! $redirect !!}</a>
+    <div class="container">
+        <div class="card successbox">
+            <h2 class="card-header">{!! $title !!}</h2>
+            <div class="card-body">
+                <strong>{!! $msg !!}</strong>
+            </div>
+            <div class="card-footer">
+                <a href="{!! $redirect_to !!}">{!! $redirect !!}</a>
+            </div>
         </div>
     </div>
 @endsection
