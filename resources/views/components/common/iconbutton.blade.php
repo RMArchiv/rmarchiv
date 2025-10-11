2b2675c80012ee42e39413b3b2d1a371f344b3a4
wes <207344724+jwesn@users.noreply.github.com>
2025-10-12 01:01:28 +0200

    fix information box border color, iconbutton icon classes & make comment rules denser

--- resources/views/components/common/iconbutton.blade.php
+++ resources/views/components/common/iconbutton.blade.php
@@ -5,7 +5,7 @@
     </a>
 @else
     <button  {{ $attributes->merge(['type' => 'button', 'class' => "p-2 btn btn-secondary d-flex flex-row align-items-center gap-2"]) }}>
-        <i {{ $attributes->merge(['class' => ' w-auto ' . $icon]) }}></i>
+        <i class="{{'w-auto ' . ($icon ?? '')}}"></i>
         <span class="{{ (isset($showtextfrom) ? 'd-none' : '') . ' d-' . (isset($showtextfrom) ? $showtextfrom . '-' : '') . 'block' }}">{{ $slot }}</span>
     </button>
 @endif
