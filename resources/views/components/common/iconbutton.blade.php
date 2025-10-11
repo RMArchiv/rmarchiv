86a08d0285dc5f7488c595802519d3608e45acd4
wes <207344724+jwesn@users.noreply.github.com>
2025-10-11 23:11:25 +0200

    add tags to game information & change markdown parsing setting html_input to strip

--- resources/views/components/common/iconbutton.blade.php
+++ resources/views/components/common/iconbutton.blade.php
@@ -1,1 +1,11 @@
-<a class="p-2 btn btn-secondary d-flex flex-row align-items-center gap-2" href="{{$href}}" ><i {{ $attributes->merge(['class' => ' w-auto '.$icon]) }}></i><span class="{{(isset($showtextfrom) ? 'd-none' : '').' d-' . (isset($showtextfrom) ? $showtextfrom . '-' : '') .'block'}}">{{$slot}}</span></a>
+@if (isset($href))
+    <a {{ $attributes->merge(['type' => 'button']) }} class="p-2 btn btn-secondary d-flex flex-row align-items-center gap-2" href="{{ $href }}">
+        <i {{ $attributes->merge(['class' => ' w-auto ' . $icon]) }}></i>
+        <span class="{{ (isset($showtextfrom) ? 'd-none' : '') . ' d-' . (isset($showtextfrom) ? $showtextfrom . '-' : '') . 'block' }}">{{ $slot }}</span>
+    </a>
+@else
+    <button  {{ $attributes->merge(['type' => 'button', 'class' => "p-2 btn btn-secondary d-flex flex-row align-items-center gap-2"]) }}>
+        <i {{ $attributes->merge(['class' => ' w-auto ' . $icon]) }}></i>
+        <span class="{{ (isset($showtextfrom) ? 'd-none' : '') . ' d-' . (isset($showtextfrom) ? $showtextfrom . '-' : '') . 'block' }}">{{ $slot }}</span>
+    </button>
+@endif
--- resources/views/components/games/information.blade.php
+++ resources/views/components/games/information.blade.php
@@ -2,7 +2,7 @@
     <div class="w-100">
 
         <div class="d-flex align-items-stretch">
-            <div class=" px-3 py-2 bg-rm-back align-self-stretch d-flex align-items-center "><i class="fa fa-gear"></i>
+            <div class=" px-1 px-lg-3 py-2 bg-rm-back align-self-stretch d-flex align-items-center "><i class="fa fa-gear"></i>
             </div>
             <a class="d-flex px-2 border-bottom border-start border-rm-back-alpha w-100 align-items-center"
                 href="{{ route('maker.show', $game->maker->id) }}">
@@ -11,14 +11,14 @@
             </a>
         </div>
         <div class="d-flex align-items-stretch">
-            <div class=" px-3 py-2 bg-rm-back align-self-stretch d-flex align-items-center ">
+            <div class=" px-1 px-lg-3 py-2 bg-rm-back align-self-stretch d-flex align-items-center ">
                 <i class="fa
-                    @if ($game->gamefiles->first()->gamefiletype->short == 'full') fa-battery
-                    @elseif($game->gamefiles->first()->gamefiletype->short == 'demo')
+                    @if ($game?->gamefiles?->first()?->gamefiletype?->short == 'full') fa-battery
+                    @elseif($game?->gamefiles?->first()?->gamefiletype?->short == 'demo')
                     fa-battery-three-quarters
-                    @elseif($game->gamefiles->first()->gamefiletype->short == 'techdemo')
+                    @elseif($game?->gamefiles?->first()?->gamefiletype?->short == 'techdemo')
                     fa-battery-half
-                    @elseif($game->gamefiles->first()->gamefiletype->short == 'ptrailer')
+                    @elseif($game?->gamefiles?->first()?->gamefiletype?->short == 'ptrailer')
                     fa-battery-quarter
                     @else
                     fa-battery-empty @endif
@@ -35,21 +35,21 @@
             </div>
         </div>
         <div class="d-flex align-items-stretch">
-            <div class=" px-3 py-2 bg-rm-back align-self-stretch d-flex align-items-center ">
+            <div class=" px-1 px-lg-3 py-2 bg-rm-back align-self-stretch d-flex align-items-center ">
                 <i class="fa fa-palette"></i>
             </div>
-            <div class="d-flex px-2 border-bottom border-start border-rm-back-alpha w-100 align-items-center text-wrap">
+            <div class="d-flex px-2 border-bottom border-start border-rm-back-alpha w-100 align-items-center text-wrap flex-wrap">
                 @foreach ($game->developers as $dev)
                     <a class="" href="{{ url('developer', $dev->developer_id) }}">{{ $dev->developer->name }}</a>
                     @if ($dev != $game->developers->last())
-                        ::
+                        <span>&nbsp::&nbsp</span>
                     @endif
                 @endforeach
             </div>
         </div>
         @if (strlen(\App\Helpers\DatabaseHelper::getReleaseDateFromGameId($game->id)) > 0)
             <div class="d-flex align-items-stretch">
-                <div class="px-3 py-2 bg-rm-back align-self-stretch d-flex align-items-center">
+                <div class="px-1 px-lg-3 py-2 bg-rm-back align-self-stretch d-flex align-items-center">
                     <i class="fa fa-calendar-days"></i>
                 </div>
                 <div class="d-flex px-2 border-bottom border-start border-rm-back-alpha w-100 align-items-center">
@@ -59,7 +59,7 @@
 
         @if ($game->website_url)
             <div class="d-flex align-items-stretch">
-                <div class=" px-3 py-2 bg-rm-back align-self-stretch d-flex align-items-center position-relative">
+                <div class=" px-1 px-lg-3 py-2 bg-rm-back align-self-stretch d-flex align-items-center position-relative">
                     <i class="fa fa-globe"></i><i style="font-size: 0.5rem; bottom:4px; right:9px;"
                         class="position-absolute fa fa-arrow-pointer"></i>
                 </div>
@@ -70,7 +70,7 @@
         @endif
         @if ($game->atelier_id)
             <div class="d-flex align-items-stretch">
-                <div class="px-3 py-2 bg-rm-back align-self-stretch d-flex align-items-center"
+                <div class="px-1 px-lg-3 py-2 bg-rm-back align-self-stretch d-flex align-items-center"
                     style="filter: brightness(1.4) grayscale(100%)">
                     <img src="https://rpg-atelier.net/template/favicon.ico" width="18px" />
                 </div>
@@ -85,7 +85,7 @@
         @endif
         @if ($game->makerpendium_article)
             <div class="d-flex align-items-stretch">
-                <div class="px-3 py-2 bg-rm-back align-self-stretch d-flex align-items-center">
+                <div class="px-1 px-lg-3 py-2 bg-rm-back align-self-stretch d-flex align-items-center">
                     <i class="fa fa-book"></i>
                 </div>
                 <div class="d-flex px-2 border-bottom border-start border-rm-back-alpha w-100 align-items-center">
@@ -98,7 +98,7 @@
             <div class="d-flex align-items-stretch">
                 <div class="d-flex align-items-center border-bottom border-start border-rm-back-alpha">
                     <div title="{{ trans('app.license') }}" aria-label="{{ trans('app.license') }}"
-                        class="px-3 py-2 bg-rm-back align-self-stretch d-flex align-items-center">
+                        class="px-1 px-lg-3 py-2 bg-rm-back align-self-stretch d-flex align-items-center">
                         <i class="fa fa-section"></i>
                     </div>
                     <div class="px-2">
@@ -117,7 +117,7 @@
             );
         @endphp
         <div class="d-flex align-items-center">
-            <div class="px-3 py-2 bg-rm-back align-self-stretch d-flex align-items-center"><i class="fa fa-meteor"></i>
+            <div class="px-1 px-lg-3 py-2 bg-rm-back align-self-stretch d-flex align-items-center"><i class="fa fa-meteor"></i>
             </div>
 
             <div class="w-100 py-1 gap-1 d-flex flex-column px-2 border-bottom border-start border-rm-back-alpha">
@@ -129,8 +129,7 @@
         </div>
 
         <div class="d-flex align-items-center">
-            <div class="px-3 py-2 bg-rm-back align-self-stretch d-flex align-items-center gap-2"
-                style="filter: brightness(1.4) grayscale(100%)">
+            <div class="px-1 px-lg-3 py-2 bg-rm-back align-self-stretch d-flex align-items-center gap-2">
                 <div class="d-flex gap-2">
                     <img style="filter: brightness(2) grayscale(100%)" src='/assets/rate_up.gif' width="18px"
                         alt='{{ trans('app.rate_up') }}' />
@@ -142,7 +141,7 @@
                     {{ @$game->votes['down'] ?? 0 }}
                 </div>
             </div>
-            <div class="w-100 d-flex flex-column gap-1 border-start border-rm-back-alpha p-2 py-1">
+            <div class="w-100 d-flex flex-column gap-1 border-bottom border-start border-rm-back-alpha p-2 py-1">
                 <div class="justify-self-start">
                     @if (@$game->votes['up'] > @$game->votes['down'])
                         <img src='/assets/rate_up.gif' width="18px" alt='good' />
@@ -169,32 +168,115 @@
             @if ($game->gamefiles->count() != 0)
                 @if ($game->maker_id == 2 ?? ($game->maker_id == 3 ?? ($game->maker_id == 6 ?? $game->maker_id == 9)))
                     <div class="d-flex align-items-center">
-                        <div class="px-3 py-2 bg-rm-back align-self-stretch d-flex align-items-center">
+                        <div class="px-1 px-lg-3 py-2 bg-rm-back align-self-stretch d-flex align-items-center">
                             <i class="fa fa-gamepad"></i>
                         </div>
 
-                        <div class="w-100 py-1 gap-1 d-flex flex-column">
+                        <div class="w-100 py-1 gap-1 d-flex flex-wrap px-2 border-bottom border-start border-rm-back-alpha flex-column">
+                            <a
+                                href=@if ($game->maker_id == 6) "{{ action('PlayerMvController@index', $game->gamefiles->first()->id) }}"
+                                    @else
+                                    "{{ action('Player2kController@index', $game->gamefiles->first()->id) }}" @endif>
                             <div class="btn btn-secondary fw-bold">
                                 {{ trans('app.play_in_browser') }}!
-                                <a
-                                    href=@if ($game->maker_id == 6) "{{ action('PlayerMvController@index', $game->gamefiles->first()->id) }}"
-                                        @else
-                                            "{{ action('Player2kController@index', $game->gamefiles->first()->id) }}" @endif>
-                                    <img src="/assets/play_button.png" alt="play"></a>
+                                <img src="/assets/play_button.png" alt="play">
                             </div>
+                            </a>
                         </div>
                     </div>
                 @endif
             @endif
         @endif
 
+        <div class="d-flex align-items-center">
+            <div title="{{ trans('app.tags') }}"
+                class="px-1 px-lg-3 py-2 bg-rm-back align-self-stretch d-flex align-items-center">
+                <i class="fa fa-tag"></i>
+            </div>
+
+            <div
+                class="w-100 py-1 gap-1 d-flex flex-row flex-wrap px-2 border-bottom border-start border-rm-back-alpha ">
+                <div>
+                    @foreach ($game->tags as $tag)
+                        <small class="badge rounded-pill bg-warning text-bg-warning"><a class="text-bg-warning"
+                                href="{{ action('TaggingController@showGames', [$tag->tag_id]) }}">{{ $tag->tag->title }}</a>
+                            @if ($tag != $game->tags->last())
+                            @endif
+                        </small>
+                    @endforeach
+                    @if (Auth::check())
+                        <div id="addtag" class="modal fade" role="dialog">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title">{{ trans('app.add_tag') }}</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <div>
+                                            <form method="POST" action="{{ action('TaggingController@store') }}"
+                                                class="form-horizontal">
+                                                @csrf
+                                                <input type="hidden" name='content_id' value="{{ $game->id }}">
+                                                <input type="hidden" name='content_type' value="{{ 'game' }}">
+                                                <fieldset>
+                                                    <div class="form-group" id="row_tag">
+                                                        <label for="tag"
+                                                            class="col-form-label">{{ trans('app.tag_name') }}</label>
+                                                        <div class="d-flex gap-4">
+                                                            <div class="autocomplete">
+                                                                <input autocomplete="off" type="text"
+                                                                    class="d-none auto form-control" id="tag"
+                                                                    name="title" value="">
+                                                                <div id="searchbar"></div>
+                                                                <div id="searchcontainer"></div>
+                                                            </div>
+                                                            <input class="btn btn-secondary w-25" type='submit'
+                                                                value='{{ trans('app.submit') }}' id='submit'>
+
+                                                        </div>
+                                                    </div>
+
+                                                    <script type="module">
+                                                        createAutocomplete({
+                                                            apiPath: () => {
+                                                                return "ac_tag"
+                                                            },
+                                                            placeholder: "{{ trans('app.search') }}",
+                                                            searchbarSelector: "#row_tag #searchbar",
+                                                            panelSelector: "#searchcontainer",
+                                                            noResults:'{{ trans('app.search_nothing_found') }}',
+                                                            type: "list",
+                                                            action: "find",
+                                                            limit: 5,
+                                                            inputSelector: ".autocomplete #tag",
+                                                            // disable detached mode because input does not work in nested bootstrap modal
+                                                            additionalProps: {
+                                                                detachedMediaQuery: "none"
+                                                            }
+                                                        })
+                                                    </script>
+                                                </fieldset>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <x-common.iconbutton class="mw-50 mt-2" showtextfrom="sm" icon="fa fa-tag" data-bs-toggle="modal" data-bs-target="#addtag">{{ trans('app.add_tag') }}</x-common.iconbutton>
+                    @endif
+                </div>
+            </div>
+        </div>
+
         <div class="card-footer d-flex justify-content-end align-items-center gap-2 px-2" style="zoom: 75%;">
-            {{ trans('app.submitted_by') }} <a href='{{ url('users', $game->user_id) }}'
-                class='user'>{{ $game->user->name }}</a>
+            <div class="d-block d-md-flex flex-column d-lg-block">{{ trans('app.submitted_by') }} <a href='{{ url('users', $game->user_id) }}'
+                class='user word-hyphens'>{{ $game->user->name }}</a>
             <a href='{{ url('users', $game->user_id) }}' class='usera' title="{{ $game->user->name }}">
                 <img width="16px" src='//{{ config('app.avatar_path') }}?gender=male&id={{ $game->user_id }}'
                     alt="{{ $game->user->name }}" class='avatar' />
-            </a>
+            </a></div>
             <small>
                 <time datetime='{{ $game->created_at }}'
                     title='{{ $game->created_at }}'>({{ \Carbon\Carbon::parse($game->created_at)->diffForHumans() }})
