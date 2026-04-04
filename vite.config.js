de82a3fda12dde36fe301865efe16e713a13598a
wes <207344724+jwesn@users.noreply.github.com>
2026-04-04 19:29:57 +0200

    replace axios & remove more unused packages

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,6 @@ export default defineConfig({
   plugins: [
     laravel({
       input: [
-        // "node_modules/typeahead.js/dist/bloodhound",
-        // "node_modules/typeahead.js/dist/typeahead.jquery.js",
-        // "node_modules/bloodhound-js/dist/bloodhound.min.js",
-        // "node_modules/editor.md/editormd",
-        // 'node_modules/inline-attachment/src/inline-attachment.js',
-        // 'node_modules/inline-attachment/src/jquery.inline-attachment.js',
-        // 'node_modules/editor.md/editormd.js',
         "resources/assets/sass/app.scss",
         "resources/assets/js/app.js",
         "resources/assets/js/ckeditorSetup.js",
